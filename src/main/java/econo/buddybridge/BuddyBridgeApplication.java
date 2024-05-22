08bbf4111be96c6f03574d55f82c83e6a0714315
Profile-exe <kang67572346@gmail.com>
2024-05-22 18:03:48 +0900

    feat: 프로젝트 초기 설정

---
+++ src/main/java/econo/buddybridge/BuddyBridgeApplication.java
@@ -0,0 +1,13 @@
+package econo.buddybridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuddyBridgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuddyBridgeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/econo/buddybridge/BuddyBridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package econo.buddybridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuddyBridgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
