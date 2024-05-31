89a476ceda7a8e669d7736de56ed652d83074578
Profile-exe <kang67572346@gmail.com>
2024-05-31 21:19:11 +0900

    feat(MemberRepository): findByEmail 메서드 추가

--- src/main/java/econo/buddybridge/member/repository/MemberRepository.java
+++ src/main/java/econo/buddybridge/member/repository/MemberRepository.java
@@ -1,7 +1,10 @@
 package econo.buddybridge.member.repository;
 
 import econo.buddybridge.member.entity.Member;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member,Long> {
+
+    Optional<Member> findByEmail(String email);
 }
