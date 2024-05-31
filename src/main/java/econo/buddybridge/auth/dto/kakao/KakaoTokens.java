ecb2aeb3aba373ae0c1db8b5b3bd2d462e2d8da8
Profile-exe <kang67572346@gmail.com>
2024-05-31 16:17:03 +0900

    feat(KakaoTokens): 카카오 인증 서버 반환값을 저장하는 응답 객체 추가

---
+++ src/main/java/econo/buddybridge/auth/dto/kakao/KakaoTokens.java
@@ -0,0 +1,30 @@
+package econo.buddybridge.auth.dto.kakao;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class KakaoTokens {
+
+    @JsonProperty("token_type")
+    private String tokenType;
+
+    @JsonProperty("access_token")
+    private String accessToken;
+
+    @JsonProperty("expires_in")
+    private int expiresIn;
+
+    @JsonProperty("refresh_token")
+    private String refreshToken;
+
+    @JsonProperty("refresh_token_expires_in")
+    private int refreshTokenExpiresIn;
+
+    @JsonProperty("scope")
+    private String scope;
+}
