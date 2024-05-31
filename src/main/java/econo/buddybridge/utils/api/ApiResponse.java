0310e05224e0afff5b1aef1baea5c6acbdf98de2
Profile-exe <kang67572346@gmail.com>
2024-05-31 22:00:11 +0900

    feat(ApiResponse): 커스텀 응답 형식 추가
    - CustomBody를 통해 다양한 형태의 Data를 담아 응답 가능
    - header를 매개변수로 가진 생성자를 통해 헤더에 정보를 담아 응답 가능

---
+++ src/main/java/econo/buddybridge/utils/api/ApiResponse.java
@@ -0,0 +1,34 @@
+package econo.buddybridge.utils.api;
+
+import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+@Getter
+public class ApiResponse<B> extends ResponseEntity<B> {
+
+    public ApiResponse(B body, HttpStatus status) {
+        super(body, status);
+    }
+
+    public ApiResponse(B body, MediaType mediaType, HttpStatus status) {
+        super(body, status);
+        this.getHeaders().setContentType(mediaType);
+    }
+
+    public ApiResponse(B body, HttpHeaders headers, HttpStatus status) {
+        super(body, headers, status);
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class CustomBody<D> implements Serializable {
+        private Boolean success;
+        private D data;
+        private Error error;
+    }
+}
