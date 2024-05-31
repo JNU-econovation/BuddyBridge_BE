f631fb31634d642bfb3dc638ca1116a13bd0cc6c
Profile-exe <kang67572346@gmail.com>
2024-05-31 16:53:27 +0900

    feat(RedirectException): Feign Client의 redirect(302)에 대한 예외처리 추가

---
+++ src/main/java/econo/buddybridge/utils/exception/RedirectException.java
@@ -0,0 +1,13 @@
+package econo.buddybridge.utils.exception;
+
+import lombok.Getter;
+
+@Getter
+public class RedirectException extends Exception {
+    private final String location;
+
+    public RedirectException(String location) {
+        super("302 Found: " + location);
+        this.location = location;
+    }
+}
