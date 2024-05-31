94752dfd8412789ea24b73f43de49ece78e9769a
Profile-exe <kang67572346@gmail.com>
2024-05-31 16:13:04 +0900

    feat(KakaoLoginParams): 카카오 OAuth 요청에 필요한 authorizationCode를 갖고 있는 클래스 추가

---
+++ src/main/java/econo/buddybridge/auth/dto/kakao/KakaoLoginParams.java
@@ -0,0 +1,23 @@
+package econo.buddybridge.auth.dto.kakao;
+
+import econo.buddybridge.auth.OAuthProvider;
+import econo.buddybridge.auth.dto.OAuthLoginParams;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class KakaoLoginParams implements OAuthLoginParams {
+
+    private String authorizationCode;
+
+    @Override
+    public OAuthProvider oAuthProvider() {
+        return OAuthProvider.KAKAO;
+    }
+
+    @Override
+    public String getCode() {
+        return authorizationCode;
+    }
+}
