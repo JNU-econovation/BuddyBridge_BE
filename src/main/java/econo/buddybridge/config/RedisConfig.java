2b6f94500c88331f7dfdf0a5b54c7bdc32c133dc
Profile-exe <kang67572346@gmail.com>
2024-05-31 22:25:54 +0900

    feat(RedisConfig): Session 구현체인 Redis에서 JSON 직렬화를 수행하도록 설정 추가
    - 기본 RedisSerializer를 json()으로 등록해 GenericJackson2JsonRedisSerializer를 사용하도록 설정

---
+++ src/main/java/econo/buddybridge/config/RedisConfig.java
@@ -0,0 +1,14 @@
+package econo.buddybridge.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.serializer.RedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisSerializer<Object> springSessionDefaultRedisSerializer() {
+        return RedisSerializer.json();  // 기본값은 JdkSerializationRedisSerializer
+    }
+}
