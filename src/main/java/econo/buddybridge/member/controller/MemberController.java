cc9438ccc1470e8d93e74be4cb66121965a98916
Profile-exe <kang67572346@gmail.com>
2024-06-26 02:41:17 +0900

    feat(MemberController): 사용자 정보 조회 API 추가
    - MemberController에 사용자 정보 조회 엔드포인트 추가
    - /user/info 경로에서 세션의 memberId를 이용해 사용자 정보를 조회하도록 구현
    - ApiResponseGenerator를 사용해 성공 응답을 생성
    - HTTP 상태 코드 200(OK)와 함께 사용자 정보 반환
    
    close #14

--- src/main/java/econo/buddybridge/member/controller/MemberController.java
+++ src/main/java/econo/buddybridge/member/controller/MemberController.java
@@ -2,7 +2,13 @@ package econo.buddybridge.member.controller;
 
 import econo.buddybridge.member.dto.MemberDto;
 import econo.buddybridge.member.service.MemberService;
+import econo.buddybridge.utils.api.ApiResponse;
+import econo.buddybridge.utils.api.ApiResponse.CustomBody;
+import econo.buddybridge.utils.api.ApiResponseGenerator;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +21,12 @@ public class MemberController {
     private final MemberService memberService;
 
     @GetMapping("/info")
-    public MemberDto findMember(Long memberId) {
-        return memberService.findMember(memberId);
+    public ApiResponse<CustomBody<MemberDto>> findMember(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        Object memberIdObj = session.getAttribute("memberId");
+
+        Long memberId = Long.valueOf(memberIdObj.toString());
+        System.out.println("memberId = " + memberId);
+        return ApiResponseGenerator.success(memberService.findMember(memberId), HttpStatus.OK);
     }
 }
