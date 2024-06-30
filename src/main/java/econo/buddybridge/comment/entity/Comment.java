c40e8350f63cf8fc55c23e57c0318db90e959dab
Profile-exe <kang67572346@gmail.com>
2024-07-01 00:43:25 +0900

    refactor(Comment): 댓글의 연관관계들 fetchType을 LAZY로 수정

--- src/main/java/econo/buddybridge/comment/entity/Comment.java
+++ src/main/java/econo/buddybridge/comment/entity/Comment.java
@@ -5,6 +5,7 @@ import econo.buddybridge.member.entity.Member;
 import econo.buddybridge.post.entity.Post;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -12,6 +13,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -28,11 +30,22 @@ public class Comment extends BaseEntity {
 
     private String content;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name="post_id")
     private Post post;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name="author_id")
     private Member author;
+
+    @Builder
+    public Comment(String content, Post post, Member author) {
+        this.content = content;
+        this.post = post;
+        this.author = author;
+    }
+
+    public void updateContent(String content) {
+        this.content = content;
+    }
 }
