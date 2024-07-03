313ac52429b610dc6346790b975fd55ddd478ee3
Profile-exe <kang67572346@gmail.com>
2024-07-03 20:59:50 +0900

    feat(MemberController): 회원정보 수정 API 구현
    close #43

--- src/main/java/econo/buddybridge/member/controller/MemberController.java
+++ src/main/java/econo/buddybridge/member/controller/MemberController.java
@@ -1,32 +1,40 @@
 package econo.buddybridge.member.controller;
 
-import econo.buddybridge.member.dto.MemberDto;
+import econo.buddybridge.member.dto.MemberReqDto;
+import econo.buddybridge.member.dto.MemberResDto;
 import econo.buddybridge.member.service.MemberService;
 import econo.buddybridge.utils.api.ApiResponse;
 import econo.buddybridge.utils.api.ApiResponse.CustomBody;
 import econo.buddybridge.utils.api.ApiResponseGenerator;
+import econo.buddybridge.utils.session.SessionUtils;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/users")
+@RequestMapping("/api/users")
 public class MemberController {
 
     private final MemberService memberService;
 
     @GetMapping("/info")
-    public ApiResponse<CustomBody<MemberDto>> findMember(HttpServletRequest request) {
-        HttpSession session = request.getSession(false);
-        Object memberIdObj = session.getAttribute("memberId");
+    public ApiResponse<CustomBody<MemberResDto>> findMember(HttpServletRequest request) {
+        Long memberId = SessionUtils.getMemberId(request);
 
-        Long memberId = Long.valueOf(memberIdObj.toString());
-        System.out.println("memberId = " + memberId);
-        return ApiResponseGenerator.success(memberService.findMember(memberId), HttpStatus.OK);
+        return ApiResponseGenerator.success(memberService.findMemberById(memberId), HttpStatus.OK);
+    }
+
+    @PutMapping("/info")
+    public ApiResponse<CustomBody<Void>> updateMember(HttpServletRequest request, @RequestBody MemberReqDto memberReqDto) {
+        Long memberId = SessionUtils.getMemberId(request);
+        memberService.updateMemberById(memberId, memberReqDto);
+
+        return ApiResponseGenerator.success(HttpStatus.OK);
     }
 }
