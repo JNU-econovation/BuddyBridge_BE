1496f18db3f80aa5a7f252fbcbb510fc7fa28832
injae-348 <ant0593@naver.com>
2024-07-06 17:18:49 +0900

    fix typo: EDUCATION -> 교육, LIVING -> 생활로 수정

--- src/main/java/econo/buddybridge/post/entity/AssistanceType.java
+++ src/main/java/econo/buddybridge/post/entity/AssistanceType.java
@@ -1,26 +1,13 @@
 package econo.buddybridge.post.entity;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonValue;
+import lombok.Getter;
 
-import java.util.Arrays;
-
+@Getter
 public enum AssistanceType {
-    EDUCATION("교육"),
-    LIVING("생활");
+    교육("교육"),
+    생활("생활");
 
     private final String assistanceType;
 
     AssistanceType(String assistanceType){ this.assistanceType = assistanceType; }
-
-    @JsonValue
-    public String getAssistanceType(){return assistanceType;}
-    
-    @JsonCreator
-    public static AssistanceType fromString(String assistanceType){
-        return Arrays.stream(AssistanceType.values())
-                .filter(val -> val.getAssistanceType().equals(assistanceType))
-                .findFirst()
-                .orElseThrow(()->new IllegalArgumentException(assistanceType + "와/과 일치하는 타입이 없습니다."));
-    }
 }
