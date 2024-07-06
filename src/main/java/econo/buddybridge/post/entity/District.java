27357df4fe32d10742a241ae36c568b8f69cabe4
JeongInJae <93825184+injae-348@users.noreply.github.com>
2024-07-06 19:27:44 +0900

    [FIX TYPO] 열거형 상수 수정 (#52)
    * fix typo: Gwangju -> 광주광역시, BUK_GU -> 북구, 영어 이름 한글로 수정
    
    * fix typo: REGULAR -> 정기, IRREGULAR -> 비정기 수정, District @Getter 추가
    
    * fix typo: EDUCATION -> 교육, LIVING -> 생활로 수정

--- src/main/java/econo/buddybridge/post/entity/District.java
+++ src/main/java/econo/buddybridge/post/entity/District.java
@@ -1,31 +1,19 @@
 package econo.buddybridge.post.entity;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonValue;
+import lombok.Getter;
 
-import java.util.Arrays;
-
+@Getter
 public enum District {
-    GWANGJU("광주광역시"),
-    BUK_GU("광주광역시 북구"),
-    NAM_GU("광주광역시 남구"),
-    SEO_GU("광주광역시 서구"),
-    DONG_GU("광주광역시 동구"),
-    GWANGSAN_GU("광주광역시 광산구");
+    광주광역시("광주광역시"),
+    북구("광주광역시 북구"),
+    남구("광주광역시 남구"),
+    서구("광주광역시 서구"),
+    동구("광주광역시 동구"),
+    광산구("광주광역시 광산구");
 
     private final String district;
 
     District(String district){
         this.district = district;
-    }
-    @JsonValue
-    public String getDistrict(){ return district; }
-
-    @JsonCreator
-    public static District fromString(String district){
-        return Arrays.stream(District.values())
-                .filter(val -> val.getDistrict().equals(district))
-                .findFirst()
-                .orElseThrow(()->new IllegalArgumentException(district + "와/과 일치하는 타입이 없습니다."));
     }
 }
