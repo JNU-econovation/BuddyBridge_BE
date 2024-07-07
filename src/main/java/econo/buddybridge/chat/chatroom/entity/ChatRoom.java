d6927e797c86a90e9d971a8564bdd7275fb6d1f8
injae-348 <ant0593@naver.com>
2024-07-07 17:22:40 +0900

    feat: ChatRoom Entity 생성

---
+++ src/main/java/econo/buddybridge/chat/chatroom/entity/ChatRoom.java
@@ -0,0 +1,42 @@
+package econo.buddybridge.chat.chatroom.entity;
+
+
+import econo.buddybridge.common.persistence.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access=AccessLevel.PROTECTED)
+@Table(name="CHAT_ROOM")
+public class ChatRoom extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // 어떤 타입들이 있는지 이야기할 필요성
+    @Enumerated(EnumType.STRING)
+    private RoomState roomState;
+
+    private String lastMessage;
+
+    private LocalDateTime lastMessageTime;
+
+    @Builder
+    public ChatRoom(RoomState roomState, String lastMessage, LocalDateTime lastMessageTime){
+        this.roomState = roomState;
+        this.lastMessage = lastMessage;
+        this.lastMessageTime = lastMessageTime;
+    }
+
+    public void updateChatRoom(RoomState roomState,String lastMessage,LocalDateTime lastMessageTime){
+        this.roomState = roomState;
+        this.lastMessage = lastMessage;
+        this.lastMessageTime = lastMessageTime;
+    }
+}
