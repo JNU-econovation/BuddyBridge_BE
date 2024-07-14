a758ca50417d90e8d3906b6626b6a023241ff0c4
injae-348 <ant0593@naver.com>
2024-07-14 17:48:45 +0900

    refactor: chatRoom 필드 제거

--- src/main/java/econo/buddybridge/matching/entity/Matching.java
+++ src/main/java/econo/buddybridge/matching/entity/Matching.java
@@ -1,6 +1,5 @@
 package econo.buddybridge.matching.entity;
 
-import econo.buddybridge.chat.chatroom.entity.ChatRoom;
 import econo.buddybridge.common.persistence.BaseEntity;
 import econo.buddybridge.member.entity.Member;
 import econo.buddybridge.post.entity.Post;
@@ -25,31 +24,23 @@ public class Matching extends BaseEntity {
     @JoinColumn(name="post_id")
     private Post post;
 
-    // 게시글 작성자 -> 고정
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "taker_id")
     private Member taker;
 
-    // 댓글 작성자 -> 고정
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "giver_id")
     private Member giver;
-
-    // 채팅방 id 필드
-    @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "room_id")
-    private ChatRoom chatRoom;
 
     // 매칭 상태
     @Enumerated(EnumType.STRING)
     private MatchingStatus matchingStatus;
 
     @Builder
-    public Matching(Post post, Member taker, Member giver, ChatRoom chatRoom, MatchingStatus matchingStatus){
+    public Matching(Post post, Member taker, Member giver, MatchingStatus matchingStatus){
         this.post = post;
         this.taker = taker;
         this.giver = giver;
-        this.chatRoom = chatRoom;
         this.matchingStatus = matchingStatus;
     }
     
