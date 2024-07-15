2de5593326e1e8a493a5b4ad3e9d50ca329eddea
injae-348 <ant0593@naver.com>
2024-07-15 12:47:13 +0900

    feat: cursor가 포함된 요청에 대한 페이지네이션 구현

--- src/main/java/econo/buddybridge/matching/repository/MatchingRepository.java
+++ src/main/java/econo/buddybridge/matching/repository/MatchingRepository.java
@@ -1,11 +1,29 @@
 package econo.buddybridge.matching.repository;
 
 import econo.buddybridge.matching.entity.Matching;
+import feign.Param;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-import java.util.List;
+import java.time.LocalDateTime;
 
 public interface MatchingRepository extends JpaRepository<Matching, Long> {
-    List<Matching> findMatchingByTakerIdOrGiverId(Long takerId, Long giverId);
 
+    @Query("SELECT m FROM Matching m " +
+            "LEFT JOIN ChatMessage cm ON m.id = cm.matching.id " +
+            "WHERE (m.taker.id = :memberId OR m.giver.id = :memberId) " +
+            "GROUP BY m.id " +
+            "ORDER BY MAX(cm.createdAt) DESC")
+    Slice<Matching> findMatchingByTakerIdOrGiverId(@Param("memberId") Long memberId, Pageable pageable);
+
+
+    @Query("SELECT m FROM Matching m " +
+            "LEFT JOIN ChatMessage cm ON m.id = cm.matching.id " +
+            "WHERE (m.taker.id = :memberId OR m.giver.id = :memberId) " +
+            "AND cm.createdAt > :cursor " +
+            "GROUP BY m.id " +
+            "ORDER BY MAX(cm.createdAt) DESC")
+    Slice<Matching> findMatchingByTakerIdOrGiverIdAndIdLessThan(@Param("memberId") Long memberId,@Param("cursor") LocalDateTime cursor, Pageable pageable);
 }
--- src/main/java/econo/buddybridge/matching/service/MatchingRoomService.java
+++ src/main/java/econo/buddybridge/matching/service/MatchingRoomService.java
@@ -3,6 +3,7 @@ package econo.buddybridge.matching.service;
 import econo.buddybridge.chat.chatmessage.dto.ChatMessageResDto;
 import econo.buddybridge.chat.chatmessage.entity.ChatMessage;
 import econo.buddybridge.chat.chatmessage.repository.ChatMessageRepository;
+import econo.buddybridge.matching.dto.MatchingCustomPage;
 import econo.buddybridge.matching.dto.MatchingResDto;
 import econo.buddybridge.matching.dto.ReceiverDto;
 import econo.buddybridge.matching.entity.Matching;
@@ -10,9 +11,13 @@ import econo.buddybridge.matching.repository.MatchingRepository;
 import econo.buddybridge.member.entity.Member;
 import econo.buddybridge.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -22,30 +27,45 @@ public class MatchingRoomService {
     private final MemberRepository memberRepository;
     private final MatchingRepository matchingRepository;
     private final ChatMessageRepository chatMessageRepository;
-    
+
+    // 페이지네이션 적용
     @Transactional
-    public List<MatchingResDto> getMatchingRoomsByMemberId(Long memberId){
-        List<Matching> matchingList = matchingRepository.findMatchingByTakerIdOrGiverId(memberId,memberId);
-        
-        return matchingList.stream()
+    public MatchingCustomPage getMatchingRoomsByMemberId(Long memberId, Integer size, LocalDateTime cursor){
+        Pageable pageable = PageRequest.of(0,size+1);
+        Slice<Matching> matchingSlice;
+
+        if(cursor == null){
+            matchingSlice = matchingRepository.findMatchingByTakerIdOrGiverId(memberId,pageable);
+        } else { // 마지막 채팅 메시지 생성일자 기준 내림차순, cursor 값 이후에 생성된 내용 조회
+            matchingSlice = matchingRepository.findMatchingByTakerIdOrGiverIdAndIdLessThan(memberId,cursor,pageable);
+        }
+
+        List<Matching> matchingList = matchingSlice.getContent();
+
+        List<MatchingResDto> matchingResDtoList = matchingList.stream().limit(size)
                 .map(matching -> {
                     Member receiver = getReceiver(matching,memberId);
                     ChatMessage lastMessage = chatMessageRepository.findLastMessageByMatchingId(matching.getId());
                     return MatchingResDto.builder()
-                        .matchingId(matching.getId())
-                        .lastMessage(lastMessage.getContent())
-                        .lastMessageTime(lastMessage.getCreatedAt())
-                        .messageType(lastMessage.getMessageType())
-                        .matchingStatus(matching.getMatchingStatus())
-                        .receiverDto(
-                                ReceiverDto.builder()
-                                    .receiverId(receiver.getId())
-                                    .receiverName(receiver.getNickname())
-                                    .receiverProfileImg(receiver.getProfileImageUrl())
-                                    .build()
-                        )
-                        .build();
+                            .matchingId(matching.getId())
+                            .lastMessage(lastMessage.getContent())
+                            .lastMessageTime(lastMessage.getCreatedAt())
+                            .messageType(lastMessage.getMessageType())
+                            .matchingStatus(matching.getMatchingStatus())
+                            .receiverDto(
+                                    ReceiverDto.builder()
+                                            .receiverId(receiver.getId())
+                                            .receiverName(receiver.getNickname())
+                                            .receiverProfileImg(receiver.getProfileImageUrl())
+                                            .build()
+                            )
+                            .build();
                 }).toList();
+
+        boolean nextPage = matchingList.size() > size;
+
+        LocalDateTime nextCursor = nextPage && !matchingResDtoList.isEmpty() ? matchingResDtoList.get(matchingResDtoList.size() - 1).lastMessageTime() : null;
+        return new MatchingCustomPage(matchingResDtoList,nextCursor,nextPage);
     }
 
     @Transactional // 메시지 조회
