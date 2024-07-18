4bfc0aad14cfe6ef99b2e6d067d9a8d624b19cb8
Profile-exe <kang67572346@gmail.com>
2024-07-19 01:28:03 +0900

    feat(EmitterRepository): SSE 이미터 리파지토리 구현
    - 동시성 문제를 고려해 `ConcurrentHashMap` 사용
    - 현재 연결된 이미터와 전송된 알림에 대한 캐시를 관리

---
+++ src/main/java/econo/buddybridge/notification/repository/EmitterRepository.java
@@ -0,0 +1,20 @@
+package econo.buddybridge.notification.repository;
+
+import java.util.Map;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+@Repository
+public interface EmitterRepository {
+
+    SseEmitter save(String emitterId, SseEmitter sseEmitter);
+
+    void saveEventCache(String eventCachedId, Object event);
+
+    Map<String, SseEmitter> findAllEmitterStartWithByMemberId(String memberId);
+
+    Map<String, Object> findAllEventCacheStartWithByMemberId(String memberId);
+
+    void deleteById(String emitterId);
+
+}
---
+++ src/main/java/econo/buddybridge/notification/repository/EmitterRepositoryImpl.java
@@ -0,0 +1,44 @@
+package econo.buddybridge.notification.repository;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+@Component
+public class EmitterRepositoryImpl implements EmitterRepository {
+
+    private final Map<String, SseEmitter> emitters = new ConcurrentHashMap<>();
+    private final Map<String, Object> eventCaches = new ConcurrentHashMap<>();
+
+    @Override
+    public SseEmitter save(String emitterId, SseEmitter sseEmitter) {
+        emitters.put(emitterId, sseEmitter);
+        return sseEmitter;
+    }
+
+    @Override
+    public void saveEventCache(String eventCachedId, Object event) {
+        eventCaches.put(eventCachedId, event);
+    }
+
+    @Override
+    public Map<String, SseEmitter> findAllEmitterStartWithByMemberId(String memberId) {
+        return emitters.entrySet().stream()
+                .filter(entry -> entry.getKey().startsWith(memberId))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
+
+    @Override
+    public Map<String, Object> findAllEventCacheStartWithByMemberId(String memberId) {
+        return eventCaches.entrySet().stream()
+                .filter(entry -> entry.getKey().startsWith(memberId))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
+
+    @Override
+    public void deleteById(String emitterId) {
+        emitters.remove(emitterId);
+    }
+}
