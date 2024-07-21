4f95785a9c7c588aaebef783b8fdaa492160c721
Profile-exe <kang67572346@gmail.com>
2024-07-22 01:56:25 +0900

    style(ChatMessageController): 컨벤션 수정 및 TODO 주석 제거

--- src/main/java/econo/buddybridge/chat/chatmessage/controller/ChatMessageController.java
+++ src/main/java/econo/buddybridge/chat/chatmessage/controller/ChatMessageController.java
@@ -4,15 +4,19 @@ import econo.buddybridge.chat.chatmessage.dto.ChatMessageReqDto;
 import econo.buddybridge.chat.chatmessage.dto.ChatMessageResDto;
 import econo.buddybridge.chat.chatmessage.service.ChatMessageService;
 import econo.buddybridge.utils.session.SessionUtils;
-import lombok.RequiredArgsConstructor;
-import org.springframework.messaging.handler.annotation.*;
-import org.springframework.web.bind.annotation.RestController;
-
 import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class ChatMessageController {
+
     private final ChatMessageService chatMessageService;
 
     @MessageMapping("/chat/{matching-id}") // 메시지 보내기 // /api/app/chat/{room-id} - pub
@@ -20,13 +24,10 @@ public class ChatMessageController {
     public ChatMessageResDto sendMessage(
             @DestinationVariable("matching-id") Long matchingId,
             @Payload ChatMessageReqDto chatMessageReqDto,
-            @Header ("simpSessionAttributes") Map<String, Object> attributes
-    ){
+            @Header("simpSessionAttributes") Map<String, Object> attributes
+    ) {
         Object memberIdObject = attributes.get("memberId");
         Long senderId = SessionUtils.validateMemberId(memberIdObject);
-        return chatMessageService.save(senderId,chatMessageReqDto,matchingId);
+        return chatMessageService.save(senderId, chatMessageReqDto, matchingId);
     }
-
-
-    //TODO: 채팅 메시지 삭제(업데이트로 MessageType: DELETE로 변경)
 }
