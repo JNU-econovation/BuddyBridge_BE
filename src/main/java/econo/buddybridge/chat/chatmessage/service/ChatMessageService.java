41aa66b24843e55de061c1b86d695c95d120dd6d
Profile-exe <kang67572346@gmail.com>
2024-07-22 01:57:08 +0900

    feat(ChatMessageService): 채팅 알림 전송 기능 추가

--- src/main/java/econo/buddybridge/chat/chatmessage/service/ChatMessageService.java
+++ src/main/java/econo/buddybridge/chat/chatmessage/service/ChatMessageService.java
@@ -8,6 +8,8 @@ import econo.buddybridge.matching.entity.Matching;
 import econo.buddybridge.matching.repository.MatchingRepository;
 import econo.buddybridge.member.entity.Member;
 import econo.buddybridge.member.repository.MemberRepository;
+import econo.buddybridge.notification.entity.NotificationType;
+import econo.buddybridge.notification.service.EmitterService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -18,14 +20,16 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class ChatMessageService {
+
     private final MemberRepository memberRepository;
     private final MatchingRepository matchingRepository;
     private final ChatMessageRepository chatMessageRepository;
+    private final EmitterService emitterService;
 
     @Transactional // 메시지 저장
     public ChatMessageResDto save(Long senderId, ChatMessageReqDto chatMessageReqDto, Long matchingId) {
         Member sender = memberRepository.findById(senderId)
-                .orElseThrow(()->new IllegalArgumentException("존재하지 않는 사용자입니다."));
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 사용자입니다."));
 
         Matching matching = matchingRepository.findById(matchingId)
                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 매칭입니다."));
@@ -36,6 +40,17 @@ public class ChatMessageService {
                 .content(chatMessageReqDto.content())
                 .messageType(chatMessageReqDto.messageType())
                 .build();
+
+        Long receiverId = getReceiverId(sender.getId(), matching.getId());
+        Member receiver = memberRepository.findById(receiverId)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 사용자입니다."));
+
+        emitterService.send(    // 채팅을 받는 사용자에게 알림 전송
+                receiver,
+                sender.getName() + "님이 메시지를 보냈습니다. - " + chatMessage.getContent(),
+                "/chat/" + matching.getId(),
+                NotificationType.CHAT
+        );
 
         chatMessageRepository.save(chatMessage);
 
@@ -48,10 +63,23 @@ public class ChatMessageService {
                 .build();
     }
 
+    private Long getReceiverId(Long senderId, Long matchingId) {
+        Matching matching = matchingRepository.findById(matchingId)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 매칭입니다."));
+
+        if (matching.getGiver().getId().equals(senderId)) {
+            return matching.getTaker().getId();
+        } else if (matching.getTaker().getId().equals(senderId)) {
+            return matching.getGiver().getId();
+        } else {
+            throw new IllegalArgumentException("매칭에 속하지 않은 사용자입니다.");
+        }
+    }
+
     @Transactional // 마지막 메시지 조회
-    public ChatMessageResDto getLastChatMessage(Long matchingId){
+    public ChatMessageResDto getLastChatMessage(Long matchingId) {
         List<ChatMessage> chatMessageList = chatMessageRepository.findLastMessageByMatchingId(matchingId, PageRequest.of(0, 1));
-        if(chatMessageList.isEmpty()) {
+        if (chatMessageList.isEmpty()) {
             throw new IllegalArgumentException("마지막 메시지가 존재하지 않습니다.");
         }
 
