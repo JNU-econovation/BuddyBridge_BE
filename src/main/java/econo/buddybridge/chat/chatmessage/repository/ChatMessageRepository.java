882538a38c955cbb7e73671c48523a7e4a42356f
JeongInJae <93825184+injae-348@users.noreply.github.com>
2024-07-22 14:46:24 +0900

    fix: ChatMessage 오름차순 정렬에서 내림차순 정렬로 변경 (#84)
    ChatMessage 오름차순 정렬을 내림차순으로 변경

--- src/main/java/econo/buddybridge/chat/chatmessage/repository/ChatMessageRepository.java
+++ src/main/java/econo/buddybridge/chat/chatmessage/repository/ChatMessageRepository.java
@@ -13,9 +13,9 @@ public interface ChatMessageRepository extends JpaRepository<ChatMessage,Long> {
     @Query("SELECT cm FROM ChatMessage cm WHERE cm.matching.id = :matchingId ORDER BY cm.id DESC")
     List<ChatMessage> findLastMessageByMatchingId(Long matchingId, Pageable pageable);
 
-    @Query("SELECT cm FROM ChatMessage cm WHERE cm.matching.id = :matchingId ORDER BY cm.id ASC")
+    @Query("SELECT cm FROM ChatMessage cm WHERE cm.matching.id = :matchingId ORDER BY cm.id DESC")
     Slice<ChatMessage> findByMatchingId(Long matchingId, Pageable pageable);
 
-    @Query("SELECT cm FROM ChatMessage cm WHERE cm.matching.id = :matchingId AND cm.id > :cursor ORDER BY cm.id ASC")
+    @Query("SELECT cm FROM ChatMessage cm WHERE cm.matching.id = :matchingId AND cm.id > :cursor ORDER BY cm.id DESC")
     Slice<ChatMessage> findByMatchingIdAndIdGreaterThan(Long matchingId, Long cursor, Pageable pageable);
 }
