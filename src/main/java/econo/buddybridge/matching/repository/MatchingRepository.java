29236cc47018c1441301349d48f4b8ca84d48fa9
JeongInJae <93825184+injae-348@users.noreply.github.com>
2024-07-24 21:44:26 +0900

    [FEAT] 매칭 상태에 따라 게시글 상태 결정 (#97)

--- src/main/java/econo/buddybridge/matching/repository/MatchingRepository.java
+++ src/main/java/econo/buddybridge/matching/repository/MatchingRepository.java
@@ -1,14 +1,10 @@
 package econo.buddybridge.matching.repository;
 
 import econo.buddybridge.matching.entity.Matching;
-import feign.Param;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
-import java.time.LocalDateTime;
+import java.util.List;
 
 public interface MatchingRepository extends JpaRepository<Matching, Long> {
-
+    List<Matching> findByPostId(Long postId);
 }
