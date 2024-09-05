fd563451ebbb3c3306e637337ee151077667f550
Profile-exe <kang67572346@gmail.com>
2024-09-05 09:46:13 +0900

    feat: 매칭 API 문서 작성

--- src/main/java/econo/buddybridge/matching/controller/MatchingController.java
+++ src/main/java/econo/buddybridge/matching/controller/MatchingController.java
@@ -6,6 +6,8 @@ import econo.buddybridge.matching.service.MatchingService;
 import econo.buddybridge.utils.api.ApiResponse;
 import econo.buddybridge.utils.api.ApiResponseGenerator;
 import econo.buddybridge.utils.session.SessionUtils;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -14,10 +16,12 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/matching")
+@Tag(name = "매칭 API", description = "매칭 관련 API")
 public class MatchingController {
 
     private final MatchingService matchingService;
 
+    @Operation(summary = "매칭 생성(수락)", description = "매칭을 생성(수락)합니다.")
     @PostMapping("/accept")
     public ApiResponse<ApiResponse.CustomBody<Long>> createMatching(
             @RequestBody MatchingReqDto matchingReqDto,
@@ -29,6 +33,7 @@ public class MatchingController {
     }
 
     // matching 완료 -> DONE 변경, 거절 -> FAILED 변경
+    @Operation(summary = "매칭 상태 변경", description = "매칭 상태를 변경합니다. 매칭 상태(DONE, FAILED, PENDING)에 따라 게시글 상태도 변경됩니다.")
     @PutMapping("/{matching-id}")
     public ApiResponse<ApiResponse.CustomBody<Long>> updateMatching(
             @PathVariable("matching-id") Long matchingId,
@@ -42,6 +47,7 @@ public class MatchingController {
 
     // matching 완전 삭제 -> 좋은 방법인가
     // 실수로 제거한 경우는 채팅방을 다시 만들어야하는지
+    @Operation(summary = "매칭 삭제", description = "매칭을 삭제합니다.")
     @DeleteMapping("/{matching-id}")
     public ApiResponse<ApiResponse.CustomBody<Void>> deleteMatching(
             @PathVariable("matching-id") Long matchingId,
