aee9ae55adc766fc0a790db894d63cca6b033a37
Profile-exe <kang67572346@gmail.com>
2024-09-05 09:38:28 +0900

    feat: 인증 API 문서 작성

--- src/main/java/econo/buddybridge/auth/controller/AuthController.java
+++ src/main/java/econo/buddybridge/auth/controller/AuthController.java
@@ -3,12 +3,13 @@ package econo.buddybridge.auth.controller;
 import econo.buddybridge.auth.OAuthProvider;
 import econo.buddybridge.auth.dto.kakao.KakaoLoginParams;
 import econo.buddybridge.auth.service.OAuthLoginService;
-import econo.buddybridge.common.annotation.AllowAnonymous;
 import econo.buddybridge.member.dto.MemberResDto;
 import econo.buddybridge.member.service.MemberService;
 import econo.buddybridge.utils.api.ApiResponse;
 import econo.buddybridge.utils.api.ApiResponse.CustomBody;
 import econo.buddybridge.utils.api.ApiResponseGenerator;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import java.net.URI;
@@ -17,12 +18,18 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/oauth")
+@Tag(name = "인증 API", description = "인증 관련 API")
 public class AuthController {
 
     private final OAuthLoginService oAuthLoginService;
@@ -31,6 +38,7 @@ public class AuthController {
     @Value("${oauth.kakao.url.front-url}")
     private String frontUrl;
 
+    @Operation(summary = "로그아웃", description = "세션을 제거합니다.")
     @GetMapping("/logout")
     public ApiResponse<CustomBody<String>> logout(HttpServletRequest request) {
         HttpSession session = request.getSession(false);
@@ -42,6 +50,7 @@ public class AuthController {
         return ApiResponseGenerator.success("이미 로그아웃 상태입니다.", HttpStatus.OK);
     }
 
+    @Operation(summary = "카카오 소셜 로그인 (코드로 로그인)", description = "Redirect URL이 백엔드 주소로 설정될 때 사용합니다.")
     @GetMapping("/login")
     public ApiResponse<CustomBody<MemberResDto>> login(@RequestParam("code") String code, HttpServletRequest request) {
         KakaoLoginParams params = new KakaoLoginParams(code);
@@ -58,6 +67,7 @@ public class AuthController {
         return ApiResponseGenerator.success(memberDto, httpHeaders, HttpStatus.PERMANENT_REDIRECT);
     }
 
+    @Operation(summary = "카카오 소셜 로그인 (토큰으로 로그인)", description = "Redirect URL이 프론트엔드 주소로 설정될 때 사용합니다.")
     @PostMapping("/login")
     public ApiResponse<CustomBody<MemberResDto>> login(@RequestBody KakaoLoginParams params, HttpServletRequest request) {
         MemberResDto memberDto = oAuthLoginService.login(params);
