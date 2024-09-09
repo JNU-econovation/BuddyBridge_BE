36b65c912c6e0029bff7cee9647f61b1040ab811
injae-348 <ant0593@naver.com>
2024-09-09 22:09:38 +0900

    feat: 좋아요 Entity, Repository 추가

---
+++ src/main/java/econo/buddybridge/post/repository/PostLikeRepository.java
@@ -0,0 +1,10 @@
+package econo.buddybridge.post.repository;
+
+import econo.buddybridge.post.entity.PostLike;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostLikeRepository extends JpaRepository<PostLike, Long> {
+
+}
