2b3a223aafff5918cd2a3b5ceb183686dc9d5cec
Profile-exe <kang67572346@gmail.com>
2024-09-09 22:10:12 +0900

    config: swagger 서버 주소 지정

--- src/main/java/econo/buddybridge/config/SwaggerConfig.java
+++ src/main/java/econo/buddybridge/config/SwaggerConfig.java
@@ -2,6 +2,10 @@ package econo.buddybridge.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+import jakarta.servlet.ServletContext;
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,11 +13,25 @@ import org.springframework.context.annotation.Configuration;
 public class SwaggerConfig {
 
     @Bean
-    public OpenAPI openAPI() {
+    public OpenAPI openAPI(ServletContext servletContext) {
+
+        String contextPath = servletContext.getContextPath();
+        Server server = new Server().url(contextPath);
+
         return new OpenAPI()
-                .info(new Info()
-                        .title("Buddy Bridge API")
-                        .description("Buddy Bridge API 명세서")
-                        .version("1.0.0"));
+                .servers(List.of(server))
+                .info(swaggerInfo());
+    }
+
+    private Info swaggerInfo() {
+        License license = new License();
+        license.setUrl("https://github.com/JNU-econovation/BuddyBridge_BE");
+        license.setName("Buddy Bridge");
+
+        return new Info()
+                .version("v0.0.1")
+                .title("Buddy Bridge API문서")
+                .description("Buddy Bridge의 API 문서 입니다.")
+                .license(license);
     }
 }
