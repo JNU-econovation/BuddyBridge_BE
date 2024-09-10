6630893dc57133f71d5bec138bbddef98304adb4
injae-348 <ant0593@naver.com>
2024-09-10 10:25:35 +0900

    refactor: 안쓰는 함수 제거

--- src/main/java/econo/buddybridge/post/repository/PostRepository.java
+++ src/main/java/econo/buddybridge/post/repository/PostRepository.java
@@ -1,13 +1,9 @@
 package econo.buddybridge.post.repository;
 
 import econo.buddybridge.post.entity.Post;
-import econo.buddybridge.post.entity.PostType;
 import lombok.NonNull;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
@@ -17,5 +13,4 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     @EntityGraph(attributePaths = {"author"})
     Optional<Post> findById(Long postId);
 
-    Page<Post> findByPostType(Pageable pageable, PostType postType);
 }
