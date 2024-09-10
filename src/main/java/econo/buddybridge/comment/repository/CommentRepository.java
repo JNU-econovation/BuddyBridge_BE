64f9c011957f0cd530a2b8497bf5b6f65daacec6
injae-348 <ant0593@naver.com>
2024-09-10 10:20:17 +0900

    refactor: @Repository 제거

--- src/main/java/econo/buddybridge/comment/repository/CommentRepository.java
+++ src/main/java/econo/buddybridge/comment/repository/CommentRepository.java
@@ -2,8 +2,6 @@ package econo.buddybridge.comment.repository;
 
 import econo.buddybridge.comment.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 }
--- src/main/java/econo/buddybridge/post/repository/PostRepository.java
+++ src/main/java/econo/buddybridge/post/repository/PostRepository.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
 
     @NonNull
