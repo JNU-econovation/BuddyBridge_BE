423ea63389a55671c78046821735653cbd029bbb
강명덕 <68066437+Profile-exe@users.noreply.github.com>
2024-09-12 15:23:14 +0900

    [FEAT] 로그아웃 HTTP method 변경 (#150)
    * feat: 로그아웃 비즈니스 예외 구현
    
    * refactor: 로그아웃 method 변경
    
    GET에서 POST로 변경
    
    * config: 프론트 URL 변경

--- src/main/java/econo/buddybridge/auth/exception/AuthErrorCode.java
+++ src/main/java/econo/buddybridge/auth/exception/AuthErrorCode.java
@@ -7,6 +7,7 @@ public enum AuthErrorCode implements ErrorCode {
     SESSION_ALREADY_EXISTS("A001", HttpStatus.BAD_REQUEST, "이미 세션이 존재합니다. 삭제 응답 후 다시 시도해주세요."),
     FEIGN_REDIRECT("A002", HttpStatus.OK, "Feign Client Logout Redirect"),
     FEIGN_KAKAO_EXCEPTION("A003", HttpStatus.BAD_REQUEST, "카카오 API 호출 오류가 발생했습니다."),
+    ALREADY_LOGOUT("A004", HttpStatus.BAD_REQUEST, "이미 로그아웃 상태입니다."),
     ;
 
     private final String code;
