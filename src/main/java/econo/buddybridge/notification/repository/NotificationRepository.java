05f2108a62fab2c86cfd8cb7358f4d48b74ba745
Profile-exe <kang67572346@gmail.com>
2024-09-13 15:01:51 +0900

    refactor: 커스텀 리파지토리 의존 관계 개선

--- src/main/java/econo/buddybridge/notification/repository/NotificationRepository.java
+++ src/main/java/econo/buddybridge/notification/repository/NotificationRepository.java
@@ -3,6 +3,6 @@ package econo.buddybridge.notification.repository;
 import econo.buddybridge.notification.entity.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface NotificationRepository extends JpaRepository<Notification, Long> {
+public interface NotificationRepository extends JpaRepository<Notification, Long>, NotificationRepositoryCustom {
 
 }
--- src/main/java/econo/buddybridge/notification/repository/NotificationRepositoryCustom.java
+++ src/main/java/econo/buddybridge/notification/repository/NotificationRepositoryCustom.java
@@ -1,9 +1,7 @@
 package econo.buddybridge.notification.repository;
 
 import econo.buddybridge.notification.dto.NotificationCustomPage;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface NotificationRepositoryCustom {
     NotificationCustomPage findByMemberId(Long memberId, Integer size, Long cursor);
 }
