9d6c588a47933d9fc4f10465ab1db44768551ffe
injae-348 <ant0593@naver.com>
2024-09-22 10:49:18 +0900

    feat: custom findById 제거, @Repository 제거, extends를 통해 PostRepositoryCustom 사용

--- src/main/java/econo/buddybridge/post/repository/PostRepositoryCustom.java
+++ src/main/java/econo/buddybridge/post/repository/PostRepositoryCustom.java
@@ -5,9 +5,7 @@ import econo.buddybridge.post.dto.PostCustomPage;
 import econo.buddybridge.post.entity.AssistanceType;
 import econo.buddybridge.post.entity.PostStatus;
 import econo.buddybridge.post.entity.PostType;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface PostRepositoryCustom {
 
     PostCustomPage findPosts(Integer page, Integer size, String sort, PostType postType,
