da8ac058c63a2b4a0282ba5d7e57da7a77fe72e3
Profile-exe <kang67572346@gmail.com>
2024-09-27 21:23:38 +0900

    refactor: 불필요한 메서드 제거

--- src/main/java/econo/buddybridge/chat/chatmessage/repository/ChatMessageRepository.java
+++ src/main/java/econo/buddybridge/chat/chatmessage/repository/ChatMessageRepository.java
@@ -3,7 +3,6 @@ package econo.buddybridge.chat.chatmessage.repository;
 import econo.buddybridge.chat.chatmessage.entity.ChatMessage;
 import java.util.List;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -11,10 +10,4 @@ public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long>,
 
     @Query("SELECT cm FROM ChatMessage cm WHERE cm.matching.id = :matchingId ORDER BY cm.id DESC")
     List<ChatMessage> findLastMessageByMatchingId(Long matchingId, Pageable pageable);
-
-    @Query("SELECT cm FROM ChatMessage cm WHERE cm.matching.id = :matchingId ORDER BY cm.id DESC")
-    Slice<ChatMessage> findByMatchingId(Long matchingId, Pageable pageable);
-
-    @Query("SELECT cm FROM ChatMessage cm WHERE cm.matching.id = :matchingId AND cm.id < :cursor ORDER BY cm.id DESC")
-    Slice<ChatMessage> findByMatchingIdAndIdGreaterThan(Long matchingId, Long cursor, Pageable pageable);
 }
