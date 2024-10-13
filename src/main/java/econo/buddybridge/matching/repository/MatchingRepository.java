0fdad97a1141b3c80c5a40ba9c375c111b42adf5
injae-348 <ant0593@naver.com>
2024-10-13 22:10:49 +0900

    refactor: 집계 함수 Native Query -> JPQL로 변경

--- src/main/java/econo/buddybridge/matching/repository/MatchingRepository.java
+++ src/main/java/econo/buddybridge/matching/repository/MatchingRepository.java
@@ -11,6 +11,6 @@ public interface MatchingRepository extends JpaRepository<Matching, Long> {
     @Query("SELECT m FROM Matching m JOIN FETCH m.post JOIN FETCH m.giver JOIN FETCH m.taker WHERE m.id = :matchingId")
     Optional<Matching> findByIdWithMembersAndPost(Long matchingId);
 
-    @Query(value = "SELECT COUNT(*) FROM MATCHING m WHERE m.post_id = :post_id AND m.matchingStatus = 'DONE'", nativeQuery = true)
+    @Query("SELECT COUNT(m) FROM Matching m WHERE m.post.id = :post_id AND m.matchingStatus = 'DONE'")
     Integer countMatchingDoneByPostId(Long post_id);
 }
