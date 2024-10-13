bf704c39cec64258da8ca24b0d4715642e7fea89
injae-348 <ant0593@naver.com>
2024-10-13 14:43:29 +0900

    feat: 매칭 완료된 수를 가져오는 네이티브 쿼리 함수 작성

--- src/main/java/econo/buddybridge/matching/repository/MatchingRepository.java
+++ src/main/java/econo/buddybridge/matching/repository/MatchingRepository.java
@@ -1,12 +1,16 @@
 package econo.buddybridge.matching.repository;
 
 import econo.buddybridge.matching.entity.Matching;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface MatchingRepository extends JpaRepository<Matching, Long> {
 
     @Query("SELECT m FROM Matching m JOIN FETCH m.post JOIN FETCH m.giver JOIN FETCH m.taker WHERE m.id = :matchingId")
     Optional<Matching> findByIdWithMembersAndPost(Long matchingId);
+
+    @Query(value = "SELECT COUNT(*) FROM MATCHING m WHERE m.post_id = :post_id AND m.matchingStatus = 'DONE'", nativeQuery = true)
+    Integer countMatchingDoneByPostId(Long post_id);
 }
