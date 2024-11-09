b5d87c9f85c7e62287307aac3076b32ef39d62c1
Profile-exe <kang67572346@gmail.com>
2024-11-10 00:43:37 +0900

    feat: JWT 사용하는 로그인 및 토큰 재발급 엔드포인트 구현

--- src/main/java/econo/buddybridge/auth/service/OAuthLoginService.java
+++ src/main/java/econo/buddybridge/auth/service/OAuthLoginService.java
@@ -3,6 +3,8 @@ package econo.buddybridge.auth.service;
 import econo.buddybridge.auth.OAuthProvider;
 import econo.buddybridge.auth.dto.OAuthLoginParams;
 import econo.buddybridge.auth.dto.kakao.UserInfoWithKakaoToken;
+import econo.buddybridge.auth.jwt.AuthToken;
+import econo.buddybridge.auth.jwt.service.AuthTokenService;
 import econo.buddybridge.member.dto.MemberResDto;
 import econo.buddybridge.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
@@ -14,10 +16,22 @@ public class OAuthLoginService {
 
     private final OAuthInfoService oAuthInfoService;
     private final MemberService memberService;
+    private final AuthTokenService authTokenService;
 
     public MemberResDto login(OAuthLoginParams params) {
         UserInfoWithKakaoToken userInfo = oAuthInfoService.getUserInfo(params);
         return memberService.findOrCreateMemberByEmail(userInfo);
+    }
+
+    // JWT를 이용한 로그인
+    public AuthToken loginWithToken(OAuthLoginParams params) {
+        UserInfoWithKakaoToken userInfo = oAuthInfoService.getUserInfo(params);
+        MemberResDto member = memberService.findOrCreateMemberByEmail(userInfo);
+        return authTokenService.generateAuthToken(member.memberId());
+    }
+
+    public AuthToken reissue(String refreshToken) {
+        return authTokenService.reissue(refreshToken);
     }
 
     public void logout(OAuthProvider provider) {
