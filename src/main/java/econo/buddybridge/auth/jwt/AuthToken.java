1a3f72ea7222adff6b36d09778994412aefc7b8e
Profile-exe <kang67572346@gmail.com>
2024-11-10 00:30:12 +0900

    feat: 토큰 응답을 위한 DTO 정의

---
+++ src/main/java/econo/buddybridge/auth/jwt/AuthToken.java
@@ -0,0 +1,13 @@
+package econo.buddybridge.auth.jwt;
+
+public record AuthToken(
+        String accessToken,
+        String refreshToken,
+        String grantType,
+        Long expiresIn
+) {
+
+    public static AuthToken of(String accessToken, String refreshToken, String grantType, Long expiresIn) {
+        return new AuthToken(accessToken, refreshToken, grantType, expiresIn);
+    }
+}
