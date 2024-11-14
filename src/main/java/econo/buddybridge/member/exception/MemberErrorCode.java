a2a3c4a7935a7d3f6c88dad06c70cd2535960fe6
injae-348 <ant0593@naver.com>
2024-11-14 23:30:49 +0900

    refactor(MemberErrorCode): 사용중인 메일입니다 -> 사용중인 이메일입니다 에러 메시지 변경

--- src/main/java/econo/buddybridge/member/exception/MemberErrorCode.java
+++ src/main/java/econo/buddybridge/member/exception/MemberErrorCode.java
@@ -5,7 +5,7 @@ import org.springframework.http.HttpStatus;
 
 public enum MemberErrorCode implements ErrorCode {
     MEMBER_NOT_FOUND("M001", HttpStatus.NOT_FOUND, "존재하지 않는 회원입니다."),
-    MEMBER_EMAIL_ALREADY_EXISTS("M002", HttpStatus.BAD_REQUEST, "이미 사용중인 메일입니다."),
+    MEMBER_EMAIL_ALREADY_EXISTS("M002", HttpStatus.BAD_REQUEST, "이미 사용중인 이메일입니다."),
     ;
 
     private final String code;
