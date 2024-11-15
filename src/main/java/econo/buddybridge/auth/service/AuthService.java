84a6dd0f965bf7cc281911236bbb3992a57d661d
injae-348 <ant0593@naver.com>
2024-11-16 00:10:57 +0900

    feat(AuthService): 입력 받은 로그인 정보로 JWT 토큰 방식 로그인 구현

--- src/main/java/econo/buddybridge/auth/service/AuthService.java
+++ src/main/java/econo/buddybridge/auth/service/AuthService.java
@@ -1,5 +1,9 @@
 package econo.buddybridge.auth.service;
 
+import econo.buddybridge.auth.dto.LoginReqDto;
+import econo.buddybridge.auth.jwt.AuthToken;
+import econo.buddybridge.auth.jwt.service.AuthTokenService;
+import econo.buddybridge.member.dto.MemberResDto;
 import econo.buddybridge.member.dto.MemberSignUpReqDto;
 import econo.buddybridge.member.dto.MemberSignUpResDto;
 import econo.buddybridge.member.service.MemberService;
@@ -11,8 +15,14 @@ import org.springframework.stereotype.Service;
 public class AuthService {
 
     private final MemberService memberService;
+    private final AuthTokenService authTokenService;
 
     public MemberSignUpResDto signUp(MemberSignUpReqDto memberSignUpReqDto) {
         return memberService.createSignUpMember(memberSignUpReqDto);
+    }
+
+    public AuthToken loginWithToken(LoginReqDto params) {
+        MemberResDto member = memberService.findMemberByEmailAndPassword(params);
+        return authTokenService.generateAuthToken(member.memberId());
     }
 }
