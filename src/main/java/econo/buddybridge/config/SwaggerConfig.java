dca2d51f2df0aecafff80a7c3348f4c7f43d81ff
강명덕 <68066437+Profile-exe@users.noreply.github.com>
2024-11-18 16:27:07 +0900

    [DOCS] Swagger JWT SecurityScheme 추가 (#199)
    docs: swagger securityScheme 추가

--- src/main/java/econo/buddybridge/config/SwaggerConfig.java
+++ src/main/java/econo/buddybridge/config/SwaggerConfig.java
@@ -1,16 +1,22 @@
 package econo.buddybridge.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import jakarta.servlet.ServletContext;
 import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 
 @Configuration
 public class SwaggerConfig {
+
+    private static final String SECURITY_SCHEME_NAME = "access-token";
 
     @Bean
     public OpenAPI openAPI(ServletContext servletContext) {
@@ -18,9 +24,22 @@ public class SwaggerConfig {
         String contextPath = servletContext.getContextPath();
         Server server = new Server().url(contextPath);
 
+        // JWT 토큰을 위한 보안 스킴 정의
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)
+                .name(HttpHeaders.AUTHORIZATION);
+
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(SECURITY_SCHEME_NAME);
+
         return new OpenAPI()
                 .servers(List.of(server))
-                .info(swaggerInfo());
+                .info(swaggerInfo())
+                .components(new Components()
+                        .addSecuritySchemes(SECURITY_SCHEME_NAME, securityScheme))
+                .addSecurityItem(securityRequirement);
     }
 
     private Info swaggerInfo() {
