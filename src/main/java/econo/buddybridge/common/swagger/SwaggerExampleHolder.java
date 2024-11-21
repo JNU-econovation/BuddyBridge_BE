573e9a2958b7d82812d1461d939b752f23c2ae43
Profile-exe <kang67572346@gmail.com>
2024-11-21 15:39:05 +0900

    feat: Swagger Example 정보를 담는 ExampleHolder 정의

---
+++ src/main/java/econo/buddybridge/common/swagger/SwaggerExampleHolder.java
@@ -0,0 +1,13 @@
+package econo.buddybridge.common.swagger;
+
+import io.swagger.v3.oas.models.examples.Example;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class SwaggerExampleHolder {
+    private Example holder;
+    private String name;
+    private int code;
+}
