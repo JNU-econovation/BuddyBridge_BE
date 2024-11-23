67bbd05efdb22d23e79f57f9098eb06872094000
injae-348 <ant0593@naver.com>
2024-11-24 00:31:48 +0900

    feat: 채팅 메시지 인가 확인 로직 작성
    ChatMessageController: @Header에서 Principal을 통해 memberId 확인
    WebConfig: excludePath 회원가입, 로그인 추가
    StompConfig: AbstractSessionWebSocketMessageBrokerConfigurer에서
    WebSocketMessageBrokerConfigurer 변경
    StompChannelInterceptor: 웹소켓 요청에 대해 jwt 검증 및 사용자 인

--- src/main/java/econo/buddybridge/chat/chatmessage/controller/ChatMessageController.java
+++ src/main/java/econo/buddybridge/chat/chatmessage/controller/ChatMessageController.java
@@ -1,33 +1,31 @@
 package econo.buddybridge.chat.chatmessage.controller;
 
-import econo.buddybridge.auth.jwt.service.JwtTokenProvider;
 import econo.buddybridge.chat.chatmessage.dto.ChatMessageReqDto;
 import econo.buddybridge.chat.chatmessage.dto.ChatMessageResDto;
 import econo.buddybridge.chat.chatmessage.service.ChatMessageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
-import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @RestController
 @RequiredArgsConstructor
 public class ChatMessageController {
 
     private final ChatMessageService chatMessageService;
-    private final JwtTokenProvider jwtTokenProvider;
 
     @MessageMapping("/chat/{matching-id}") // 메시지 보내기 // /api/app/chat/{room-id} - pub
     @SendTo("/api/queue/chat/{matching-id}") // 구독 경로 - sub
     public ChatMessageResDto sendMessage(
             @DestinationVariable("matching-id") Long matchingId,
             @Payload ChatMessageReqDto chatMessageReqDto,
-            @Header("Authorization") String header
+            Principal principal
     ) {
-        String accessToken = jwtTokenProvider.extractToken(header);
-        Long senderId = jwtTokenProvider.getMemberIdFromAccessToken(accessToken);
+        Long senderId = Long.parseLong(principal.getName());
         return chatMessageService.save(senderId, chatMessageReqDto, matchingId);
     }
 }
