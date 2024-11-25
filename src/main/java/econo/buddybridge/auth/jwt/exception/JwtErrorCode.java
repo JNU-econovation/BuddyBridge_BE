89428bbdf0f92dfe9e201b885ca9b5b1977c1227
injae-348 <ant0593@naver.com>
2024-11-25 20:59:34 +0900

    fix(JwtErrorCode): '유효하지 않은 토큰입니다. 다시로그인해주세요.'에서 '이미 로그아웃된 상태입니다.' 에러 메시지 변경

--- src/main/java/econo/buddybridge/auth/jwt/exception/JwtErrorCode.java
+++ src/main/java/econo/buddybridge/auth/jwt/exception/JwtErrorCode.java
@@ -8,7 +8,7 @@ public enum JwtErrorCode implements ErrorCode {
     INVALID_REFRESH_TOKEN("JW002", HttpStatus.UNAUTHORIZED, "올바른 REFRESH 토큰이 아닙니다."),
     EXPIRED_TOKEN("JW003", HttpStatus.UNAUTHORIZED, "토큰이 만료되었습니다."),
     MISSING_TOKEN("JW004", HttpStatus.UNAUTHORIZED, "요청에 토큰이 포함되어있지 않습니다."),
-    LOGGED_OUT_TOKEN("JW005", HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다. 다시 로그인해주세요."),
+    LOGGED_OUT_TOKEN("JW005", HttpStatus.UNAUTHORIZED, "이미 로그아웃된 상태입니다."),
     ;
 
     private final String code;
