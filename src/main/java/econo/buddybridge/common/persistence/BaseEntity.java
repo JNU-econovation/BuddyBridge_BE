0510ef6c15dec03de775cbc4f565d8c578175f81
Profile-exe <kang67572346@gmail.com>
2024-12-01 01:42:42 +0900

    feat(ReportRequest): 신고 요청을 위한 DTO 정의
    - 신고 유형이 '기타'인 경우 반드시 신고 사유를 입력해야 함
    - 신고 사유는 없더라도 빈 문자열이 되어야지 NULL이 올 수 없음

--- src/main/java/econo/buddybridge/common/persistence/BaseEntity.java
+++ src/main/java/econo/buddybridge/common/persistence/BaseEntity.java
@@ -4,7 +4,9 @@ import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -12,6 +14,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class BaseEntity {
 
     @CreatedDate
---
+++ src/main/java/econo/buddybridge/report/dto/ReportRequest.java
@@ -0,0 +1,21 @@
+package econo.buddybridge.report.dto;
+
+import econo.buddybridge.common.validation.EnumTypeValue;
+import econo.buddybridge.report.entity.ReportType;
+import jakarta.validation.constraints.AssertTrue;
+import jakarta.validation.constraints.NotNull;
+
+public record ReportRequest(
+        @EnumTypeValue(enumClass = ReportType.class)
+        String reportType,
+
+        @NotNull(message = "신고 사유는 NULL일 수 없습니다.")
+        String reportReason
+) {
+
+    @AssertTrue(message = "기타 신고 사유는 반드시 입력해야 합니다.")
+    private boolean isEtcContentNotEmpty() {
+        return !ReportType.기타.getValue().equals(reportType) ||
+                (reportReason != null && !reportReason.isBlank());
+    }
+}
