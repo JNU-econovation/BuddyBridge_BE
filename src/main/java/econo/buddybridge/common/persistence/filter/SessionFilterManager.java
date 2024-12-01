77d1c3b13693fa0b1391c452ee6ee2dad5274246
Profile-exe <kang67572346@gmail.com>
2024-12-01 23:03:52 +0900

    feat(SessionFilterManager): hibernate session의 filter 등록 및 해제를 담당하는 클래스 구현

---
+++ src/main/java/econo/buddybridge/common/persistence/filter/SessionFilterManager.java
@@ -0,0 +1,35 @@
+package econo.buddybridge.common.persistence.filter;
+
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.Session;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class SessionFilterManager {
+
+    private final EntityManager entityManager;
+
+    /**
+     * 필터 활성화
+     *
+     * @param filter 활성화 할 필터
+     * @param paramName 파라미터 이름
+     * @param paramValue 파라미터 값
+     */
+    public void enableFilter(String filter, String paramName, Object paramValue) {
+        Session session = entityManager.unwrap(Session.class);
+        session.enableFilter(filter).setParameter(paramName, paramValue);
+    }
+
+    /**
+     * 필터 비활성화
+     *
+     * @param filter 비활성화 할 필터
+     */
+    public void disableFilter(String filter) {
+        Session session = entityManager.unwrap(Session.class);
+        session.disableFilter(filter);
+    }
+}
