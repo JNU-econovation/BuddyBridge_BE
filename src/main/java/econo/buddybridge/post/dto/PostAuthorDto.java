9c70b85b149b771e8fdc2983264baa5ba43d654c
injae-348 <ant0593@naver.com>
2024-12-01 16:50:42 +0900

    fix: 게시글 작성 당시 사용자 정보(나이, 장애유형, 성별) 반환

---
+++ src/main/java/econo/buddybridge/post/dto/PostAuthorDto.java
@@ -0,0 +1,16 @@
+package econo.buddybridge.post.dto;
+
+import econo.buddybridge.member.entity.DisabilityType;
+import econo.buddybridge.member.entity.Gender;
+import lombok.Builder;
+
+@Builder
+public record PostAuthorDto(
+        Long memberId,
+        String nickname,
+        String profileImageUrl,
+        Integer age,
+        Gender gender,
+        DisabilityType disabilityType
+) {
+}
--- src/main/java/econo/buddybridge/post/dto/PostDetailDto.java
+++ src/main/java/econo/buddybridge/post/dto/PostDetailDto.java
@@ -1,20 +1,26 @@
 package econo.buddybridge.post.dto;
 
-import econo.buddybridge.member.dto.MemberResDto;
 import econo.buddybridge.post.entity.Post;
 import econo.buddybridge.post.entity.PostStatus;
 import lombok.Builder;
 
 @Builder
 public record PostDetailDto(
-        MemberResDto author,
+        PostAuthorDto author,
         PostDetailInfoDto post,
         Boolean isLiked
 ) {
 
     public PostDetailDto(Post post, Boolean isLiked, PostStatus postStatus) {
         this(
-                new MemberResDto(post.getAuthor()),
+                PostAuthorDto.builder()
+                        .memberId(post.getAuthor().getId())
+                        .nickname(post.getAuthor().getNickname())
+                        .profileImageUrl(post.getAuthor().getProfileImageUrl())
+                        .age(post.getAge())
+                        .gender(post.getGender())
+                        .disabilityType(post.getDisabilityType())
+                        .build(),
                 PostDetailInfoDto.builder()
                         .id(post.getId())
                         .title(post.getTitle())
