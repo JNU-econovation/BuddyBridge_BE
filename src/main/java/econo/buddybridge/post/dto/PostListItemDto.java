6c10464a7ccd15637885610674cb464e47b51b03
Profile-exe <kang67572346@gmail.com>
2024-12-04 15:50:37 +0900

    refactor: PostListItemDto에 PostListDto 필드 적용 및 PostListDto 제거

--- src/main/java/econo/buddybridge/post/dto/PostListItemDto.java
+++ src/main/java/econo/buddybridge/post/dto/PostListItemDto.java
@@ -1,39 +1,44 @@
 package econo.buddybridge.post.dto;
 
+import econo.buddybridge.member.entity.DisabilityType;
+import econo.buddybridge.post.entity.District;
 import econo.buddybridge.post.entity.Post;
 import econo.buddybridge.post.entity.PostStatus;
+import econo.buddybridge.post.entity.PostType;
 import lombok.Builder;
 
 @Builder
 public record PostListItemDto(
-        PostListDto post
+        Long id,
+        String title,
+        District district,
+        PostType postType,
+        PostStatus postStatus,
+        DisabilityType disabilityType,
+        AssistanceResDto assistance,
+        ScheduleListResDto schedule,
+        Boolean isLiked
 ) {
 
     public PostListItemDto(Post post, Boolean isLiked, PostStatus postStatus) {
         this(
-                PostListDto.builder()
-                        .id(post.getId())
-                        .title(post.getTitle())
-                        .district(post.getDistrict())
-                        .postType(post.getPostType())
-                        .postStatus(postStatus)
-                        .disabilityType(post.getDisabilityType())
-                        .assistance(
-                                AssistanceResDto.builder()
-                                        .assistanceType(post.getAssistanceType())
-                                        .assistanceStartTime(post.getAssistanceTime().getAssistanceStartTime())
-                                        .assistanceEndTime(post.getAssistanceTime().getAssistanceEndTime())
-                                        .build()
-                        )
-                        .schedule(
-                                ScheduleListResDto.builder()
-                                        .startDate(post.getSchedule().getStartDate())
-                                        .endDate(post.getSchedule().getEndDate())
-                                        .scheduleType(post.getSchedule().getScheduleType())
-                                        .build()
-                        )
-                        .isLiked(isLiked)
-                        .build()
+                post.getId(),
+                post.getTitle(),
+                post.getDistrict(),
+                post.getPostType(),
+                postStatus,
+                post.getDisabilityType(),
+                AssistanceResDto.builder()
+                        .assistanceType(post.getAssistanceType())
+                        .assistanceStartTime(post.getAssistanceTime().getAssistanceStartTime())
+                        .assistanceEndTime(post.getAssistanceTime().getAssistanceEndTime())
+                        .build(),
+                ScheduleListResDto.builder()
+                        .startDate(post.getSchedule().getStartDate())
+                        .endDate(post.getSchedule().getEndDate())
+                        .scheduleType(post.getSchedule().getScheduleType())
+                        .build(),
+                isLiked
         );
     }
 }
