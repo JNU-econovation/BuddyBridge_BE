49bb4754caa9906a13323ca85e6615c430792a48
injae-348 <ant0593@naver.com>
2024-12-22 17:47:39 +0900

    refactor(StompChannelInterceptor): 사용 안하는 상수 및 로그 제거

--- src/main/java/econo/buddybridge/config/StompChannelInterceptor.java
+++ src/main/java/econo/buddybridge/config/StompChannelInterceptor.java
@@ -5,7 +5,6 @@ import econo.buddybridge.auth.jwt.TokenType;
 import econo.buddybridge.auth.jwt.service.JwtTokenProvider;
 import econo.buddybridge.websocket.WebSocketPrincipal;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.simp.stomp.StompCommand;
@@ -16,12 +15,10 @@ import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-@Slf4j
 public class StompChannelInterceptor implements ChannelInterceptor {
 
     private final JwtTokenProvider jwtTokenProvider;
 
-    private final static String PREFIX = "Bearer ";
     private final static String AUTHORIZATION = "Authorization";
 
     @Override // 커스텀 헤더의 JWT를 가져옴
@@ -38,7 +35,6 @@ public class StompChannelInterceptor implements ChannelInterceptor {
     private void processAuthentication(StompHeaderAccessor accessor) {
 
         if (StompCommand.CONNECT.equals(accessor.getCommand())) {
-            log.info("STOMP COMMAND : {}", accessor.getCommand());
             Long memberId = validateAndGetMemberId(accessor);
             setPrincipal(accessor, memberId);
         }
@@ -55,7 +51,7 @@ public class StompChannelInterceptor implements ChannelInterceptor {
     }
 
     private void setPrincipal(StompHeaderAccessor accessor, Long memberId) {
-        
+
         if (accessor.getUser() == null) {
             accessor.setUser(WebSocketPrincipal.of(memberId));
         }
