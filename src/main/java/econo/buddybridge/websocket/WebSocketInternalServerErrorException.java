e5985c047fd41b4a0cee5f0c2339874aa4d1b3bc
injae-348 <ant0593@naver.com>
2024-12-24 14:41:35 +0900

    feat(WebSocketErrorCode): 웹소켓 비즈니스 에러 코드 작성

---
+++ src/main/java/econo/buddybridge/websocket/WebSocketInternalServerErrorException.java
@@ -0,0 +1,12 @@
+package econo.buddybridge.websocket;
+
+import econo.buddybridge.common.exception.BusinessException;
+
+public class WebSocketInternalServerErrorException extends BusinessException {
+
+    public static final BusinessException EXCEPTION = new WebSocketInternalServerErrorException();
+
+    private WebSocketInternalServerErrorException() {
+        super(WebSocketErrorCode.WS_INTERNAL_SERVER_ERROR);
+    }
+}
