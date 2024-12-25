781f7d49bd8d361bad5c2649be260f92ffed221d
injae-348 <ant0593@naver.com>
2024-12-25 16:54:01 +0900

    feat(ChatErrorHandler): 예외 응답 형태 적용 및 불필요한 로그 제거

--- src/main/java/econo/buddybridge/websocket/ChatErrorHandler.java
+++ src/main/java/econo/buddybridge/websocket/ChatErrorHandler.java
@@ -1,60 +1,80 @@
 package econo.buddybridge.websocket;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import econo.buddybridge.chat.chatmessage.entity.MessageType;
 import econo.buddybridge.common.exception.BusinessException;
 import econo.buddybridge.common.exception.ErrorCode;
+import econo.buddybridge.websocket.dto.WebSocketErrorResponseDto;
+import econo.buddybridge.websocket.exception.WebSocketErrorCode;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
+import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.socket.messaging.StompSubProtocolErrorHandler;
 
 import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
 
 @Component
 @Slf4j
 public class ChatErrorHandler extends StompSubProtocolErrorHandler {
 
-    public ChatErrorHandler() {
+    private final ObjectMapper objectMapper;
+
+    public ChatErrorHandler(ObjectMapper objectMapper) {
         super();
+        this.objectMapper = objectMapper;
     }
 
     @Override
     public Message<byte[]> handleClientMessageProcessingError(Message<byte[]> clientMessage, Throwable ex) {
-        log.error("Exception type(예외 타입): {}", ex.getClass().getName());
-        if (ex.getCause() != null) {
-            log.error("Cause type(커스텀 예외 타입): {}", ex.getCause().getClass().getName());
+        try {
+            ErrorCode errorCode = getErrorCode(ex);
+            WebSocketErrorResponseDto errorResponseDto = toErrorResponse(errorCode);
+
+            return buildErrorMessage(errorResponseDto);
+        } catch (JsonProcessingException e) {
+            log.error("JsonProcessingException Error(예외): {}", e.getMessage());
+            return super.handleClientMessageProcessingError(clientMessage, ex);
         }
-        log.error("Original error message(원본 에러 메시지): {}", ex.getMessage());
-
-        ErrorCode errorCode = getErrorCode(ex);
-
-        StompHeaderAccessor errorAccessor = StompHeaderAccessor.create(StompCommand.ERROR);
-        errorAccessor.setMessage(errorCode.getMessage());
-        errorAccessor.setNativeHeader("code", errorCode.getCode());
-        errorAccessor.setNativeHeader("status", String.valueOf(errorCode.getHttpStatus().value()));
-        errorAccessor.setNativeHeader("messageType", MessageType.ERROR.getMessageType());
-
-        return MessageBuilder.createMessage(
-                errorCode.getMessage().getBytes(StandardCharsets.UTF_8),
-                errorAccessor.getMessageHeaders()
-        );
     }
 
     private ErrorCode getErrorCode(Throwable ex) {
         Throwable current = ex;
+        log.error("STOMP 예외 발생: {}", ex.getMessage());
 
         while (current != null) {
             if (current instanceof BusinessException businessException) {
-                log.error("BusinessException Error(커스텀 비즈니스 예외): {}", businessException.getErrorCode().getMessage());
                 return businessException.getErrorCode();
             }
             current = current.getCause();
         }
 
-        log.error("WebSocket Error : {}", WebSocketErrorCode.WS_INTERNAL_SERVER_ERROR.getMessage());
         return WebSocketErrorCode.WS_INTERNAL_SERVER_ERROR;
+    }
+
+    private static WebSocketErrorResponseDto toErrorResponse(ErrorCode errorCode) {
+        return WebSocketErrorResponseDto.builder()
+                .code(errorCode.getCode())
+                .message(errorCode.getMessage())
+                .httpStatus(errorCode.getHttpStatus())
+                .messageType(MessageType.ERROR)
+                .createdAt(LocalDateTime.now())
+                .build();
+    }
+
+    private Message<byte[]> buildErrorMessage(WebSocketErrorResponseDto errorResponseDto) throws JsonProcessingException {
+        StompHeaderAccessor errorAccessor = StompHeaderAccessor.create(StompCommand.ERROR);
+        errorAccessor.setContentType(MimeTypeUtils.APPLICATION_JSON);
+        String jsonError = objectMapper.writeValueAsString(errorResponseDto);
+
+        return MessageBuilder.createMessage(
+                jsonError.getBytes(StandardCharsets.UTF_8),
+                errorAccessor.getMessageHeaders()
+        );
     }
 }
