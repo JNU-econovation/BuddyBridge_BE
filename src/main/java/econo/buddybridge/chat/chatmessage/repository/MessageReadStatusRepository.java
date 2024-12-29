4961ab58125f660eb5cd9e19340f807d6ac638c6
Profile-exe <kang67572346@gmail.com>
2024-12-30 00:13:55 +0900

    feat: 메시지 읽음 상태 엔티티의 리파지토리 정의 및 비즈니스 로직 구현

---
+++ src/main/java/econo/buddybridge/chat/chatmessage/repository/MessageReadStatusRepository.java
@@ -0,0 +1,12 @@
+package econo.buddybridge.chat.chatmessage.repository;
+
+import econo.buddybridge.chat.chatmessage.entity.MessageReadStatus;
+import econo.buddybridge.matching.entity.Matching;
+import econo.buddybridge.member.entity.Member;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MessageReadStatusRepository extends JpaRepository<MessageReadStatus, Long> {
+
+    Optional<MessageReadStatus> findByMatchingAndReader(Matching matching, Member reader);
+}
