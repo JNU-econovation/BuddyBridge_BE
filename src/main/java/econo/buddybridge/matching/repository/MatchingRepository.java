01d36d5593fa1e4a1844a786b888fbb4c00f8a82
injae-348 <ant0593@naver.com>
2024-12-31 19:09:05 +0900

    refactor(MatchingRepository): MatchingRepository에 CustomRepository Interface 추가

--- src/main/java/econo/buddybridge/matching/repository/MatchingRepository.java
+++ src/main/java/econo/buddybridge/matching/repository/MatchingRepository.java
@@ -1,13 +1,14 @@
 package econo.buddybridge.matching.repository;
 
 import econo.buddybridge.matching.entity.Matching;
-import java.util.List;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-public interface MatchingRepository extends JpaRepository<Matching, Long> {
+import java.util.List;
+import java.util.Optional;
+
+public interface MatchingRepository extends JpaRepository<Matching, Long>, MatchingRepositoryCustom {
 
     @Query("SELECT m FROM Matching m JOIN FETCH m.post JOIN FETCH m.giver JOIN FETCH m.taker WHERE m.id = :matchingId")
     Optional<Matching> findByIdWithMembersAndPost(Long matchingId);
--- src/main/java/econo/buddybridge/matching/repository/MatchingRepositoryCustom.java
+++ src/main/java/econo/buddybridge/matching/repository/MatchingRepositoryCustom.java
@@ -2,11 +2,10 @@ package econo.buddybridge.matching.repository;
 
 import econo.buddybridge.matching.dto.MatchingCustomPage;
 import econo.buddybridge.matching.entity.MatchingStatus;
-import java.time.LocalDateTime;
 import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Repository;
 
-@Repository
+import java.time.LocalDateTime;
+
 public interface MatchingRepositoryCustom {
 
     MatchingCustomPage findMatchings(Long memberId, Integer size, LocalDateTime cursor, MatchingStatus matchingStatus, Pageable pageable);
