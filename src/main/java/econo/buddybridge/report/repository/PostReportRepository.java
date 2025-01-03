5eef2b51b16812a0612691533ff56003c858aab6
Profile-exe <kang67572346@gmail.com>
2025-01-04 01:05:59 +0900

    feat(PostReportRepository): 게시글 다중 삭제를 위한 다중 조회 메서드 정의

--- src/main/java/econo/buddybridge/report/repository/PostReportRepository.java
+++ src/main/java/econo/buddybridge/report/repository/PostReportRepository.java
@@ -3,11 +3,14 @@ package econo.buddybridge.report.repository;
 import econo.buddybridge.member.entity.Member;
 import econo.buddybridge.post.entity.Post;
 import econo.buddybridge.report.entity.PostReport;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PostReportRepository extends JpaRepository<PostReport, Long> {
 
     boolean existsByReportedPostAndReporter(Post reportedPost, Member reporter);
 
-    boolean existsByReportedPost(Post reportedPost);
+    @Query("SELECT pr.reportedPost FROM PostReport pr WHERE pr.reportedPost IN :reportedPosts")
+    List<Post> findByReportedPostIn(List<Post> reportedPosts);
 }
