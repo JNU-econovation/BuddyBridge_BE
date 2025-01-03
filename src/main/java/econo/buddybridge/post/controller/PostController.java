56d33b67c93ba44b6201cf0bb0198532226d914a
Profile-exe <kang67572346@gmail.com>
2025-01-04 01:09:48 +0900

    feat(PostController): 게시글 다중 삭제 엔드포인트 구현

--- src/main/java/econo/buddybridge/post/controller/PostController.java
+++ src/main/java/econo/buddybridge/post/controller/PostController.java
@@ -8,6 +8,7 @@ import econo.buddybridge.post.dto.PostDetailDto;
 import econo.buddybridge.post.dto.PostEnumResDto;
 import econo.buddybridge.post.dto.PostReqDto;
 import econo.buddybridge.post.dto.PostUpdateReqDto;
+import econo.buddybridge.post.dto.PostsDeleteRequest;
 import econo.buddybridge.post.entity.AssistanceType;
 import econo.buddybridge.post.entity.PostStatus;
 import econo.buddybridge.post.entity.PostType;
@@ -135,6 +136,16 @@ public class PostController {
             @Parameter(hidden = true) @MemberTokenId Long memberId
     ) {
         postService.deletePost(postId, memberId);
+        return ApiResponseGenerator.success(HttpStatus.NO_CONTENT);
+    }
+
+    @Operation(summary = "게시글 다중 삭제", description = "게시글을 다중 삭제합니다.")
+    @DeleteMapping
+    public ApiResponse<ApiResponse.CustomBody<Void>> deletePosts(
+            @RequestBody @Valid PostsDeleteRequest postsDeleteRequest,
+            @Parameter(hidden = true) @MemberTokenId Long memberId
+    ) {
+        postService.deletePosts(postsDeleteRequest.postIds(), memberId);
         return ApiResponseGenerator.success(HttpStatus.NO_CONTENT);
     }
 }
