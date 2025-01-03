7c5802c6586389133fe17ab66be0da58f865c8fe
Profile-exe <kang67572346@gmail.com>
2025-01-03 21:14:37 +0900

    refactor(DisabilityType): ValueEnum 상속받도록 변경
    - DTO에서 유효성 검사 및 비즈니스 예외 응답 가능

--- src/main/java/econo/buddybridge/member/entity/DisabilityType.java
+++ src/main/java/econo/buddybridge/member/entity/DisabilityType.java
@@ -1,9 +1,12 @@
 package econo.buddybridge.member.entity;
 
+import econo.buddybridge.common.validation.ValueEnum;
+import econo.buddybridge.member.exception.DisabilityInvalidTypeException;
+import java.util.Arrays;
 import lombok.Getter;
 
 @Getter
-public enum DisabilityType {
+public enum DisabilityType implements ValueEnum<String> {
     없음("없음"),
     시각장애("시각장애"),
     청각장애("청각장애"),
@@ -13,9 +16,21 @@ public enum DisabilityType {
     뇌병변장애("뇌병변장애"),
     정신장애("정신장애");
 
-    private final String disabilityName;
+    private final String value;
 
-    DisabilityType(String disabilityName) {
-        this.disabilityName = disabilityName;
+    DisabilityType(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String getValue() {
+        return value;
+    }
+
+    public static DisabilityType fromValue(String value) {
+        return Arrays.stream(DisabilityType.values())
+                .filter(disabilityType -> disabilityType.getValue().equals(value))
+                .findFirst()
+                .orElseThrow(() -> DisabilityInvalidTypeException.EXCEPTION);
     }
 }
