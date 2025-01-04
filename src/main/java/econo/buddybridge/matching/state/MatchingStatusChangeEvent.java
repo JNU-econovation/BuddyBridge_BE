01792f0278a6843badbca729c38d9e10a01bed21
injae-348 <ant0593@naver.com>
2025-01-04 16:27:51 +0900

    refactor(MatchingStatusChangeEvent): ValueEnum 구현체 적용

--- src/main/java/econo/buddybridge/matching/state/MatchingStatusChangeEvent.java
+++ src/main/java/econo/buddybridge/matching/state/MatchingStatusChangeEvent.java
@@ -1,8 +1,34 @@
 package econo.buddybridge.matching.state;
 
-public enum MatchingStatusChangeEvent {
-    TOGGLE_DONE, // PENDING <-> DONE - 매칭 중 or 매칭 완료 클릭 시
-    MARK_AS_HELP_NOT_RECEIVED, // DONE -> FAILED (Only TAKER) - 도움을 받지 못했어요. 클릭 시
-    MARK_AS_HELP_RECEIVED, // DDONE -> VOLUNTEERING_COMPLETED (Only TAKER) - 도움을 받았어요! 클릭 시
-    SUBMIT_VOLUNTEERING_VERIFICATION // VOLUNTEERING_COMPLETED -> VOLUNTEERING_VERIFIED (Only GIVER) - 봉사 인증 폼 제출 시
+import econo.buddybridge.common.validation.ValueEnum;
+import econo.buddybridge.matching.exception.state.MatchingStatusChangeEventInvalidException;
+import java.util.Arrays;
+
+public enum MatchingStatusChangeEvent implements ValueEnum<String> {
+    TOGGLE_DONE("TOGGLE_DONE"), // PENDING <-> DONE - 매칭 중 or 매칭 완료 클릭 시
+    MARK_AS_HELP_NOT_RECEIVED(
+            "MARK_AS_HELP_NOT_RECEIVED"),
+    MARK_AS_HELP_RECEIVED(
+            "MARK_AS_HELP_RECEIVED"), // DDONE -> VOLUNTEERING_COMPLETED (Only TAKER) - 도움을 받았어요! 클릭 시
+    SUBMIT_VOLUNTEERING_VERIFICATION(
+            "SUBMIT_VOLUNTEERING_VERIFICATION") // VOLUNTEERING_COMPLETED -> VOLUNTEERING_VERIFIED (Only GIVER) - 봉사 인증 폼 제출 시
+    ;
+
+    private final String value;
+
+    MatchingStatusChangeEvent(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String getValue() {
+        return value;
+    }
+
+    public static MatchingStatusChangeEvent fromValue(String value) {
+        return Arrays.stream(MatchingStatusChangeEvent.values())
+                .filter(matchingStatusChangeEvent -> matchingStatusChangeEvent.getValue().equals(value))
+                .findFirst()
+                .orElseThrow(() -> MatchingStatusChangeEventInvalidException.EXCEPTION);
+    }
 }
