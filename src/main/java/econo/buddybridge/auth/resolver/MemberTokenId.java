24c7ed21d02f622f961a0f06a7ae4fa1bc4a61e1
Profile-exe <kang67572346@gmail.com>
2025-01-04 19:57:11 +0900

    feat(MemberTokenId): 인가 로직을 위해 required와 allowedRoles 추가

--- src/main/java/econo/buddybridge/auth/resolver/MemberTokenId.java
+++ src/main/java/econo/buddybridge/auth/resolver/MemberTokenId.java
@@ -1,5 +1,6 @@
 package econo.buddybridge.auth.resolver;
 
+import econo.buddybridge.member.entity.Role;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -8,5 +9,6 @@ import java.lang.annotation.Target;
 @Target(ElementType.PARAMETER)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface MemberTokenId {
-
+    boolean required() default true;
+    Role[] allowedRoles() default {Role.USER, Role.ADMIN};
 }
