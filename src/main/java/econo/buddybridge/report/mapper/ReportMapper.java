e72ba23e0d77101ce99cad8b2d2fb77220a45657
Profile-exe <kang67572346@gmail.com>
2025-01-04 20:08:23 +0900

    feat(ReportMapper): 변환 처리를 위한 Mapper 클래스 구현

---
+++ src/main/java/econo/buddybridge/report/mapper/ReportMapper.java
@@ -0,0 +1,46 @@
+package econo.buddybridge.report.mapper;
+
+import econo.buddybridge.report.dto.ReportListItem;
+import econo.buddybridge.report.entity.CommentReport;
+import econo.buddybridge.report.entity.MatchingReport;
+import econo.buddybridge.report.entity.PostReport;
+import econo.buddybridge.report.entity.Report;
+import econo.buddybridge.report.exception.ReportUnexpectedConvertException;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public final class ReportMapper {
+
+    private static final String REPORT_CONTENT_FORMAT = "%s - %s";
+
+    public static ReportListItem toReportListItem(Report report) {
+        Long postId;
+        String reportedContent;
+
+        switch (report) {
+            case PostReport postReport -> {
+                postId = postReport.getReportedPost().getId();
+                reportedContent = String.format(REPORT_CONTENT_FORMAT, "게시글", postReport.getReportedPost().getTitle());
+            }
+            case CommentReport commentReport -> {
+                postId = commentReport.getReportedComment().getPost().getId();
+                reportedContent = String.format(REPORT_CONTENT_FORMAT, "댓글", commentReport.getReportedComment().getContent());
+            }
+            case MatchingReport matchingReport -> {
+                postId = matchingReport.getReportedMatching().getId();
+                reportedContent = String.format(REPORT_CONTENT_FORMAT, "채팅방", matchingReport.getReportedMatching().getId());
+            }
+            default -> throw ReportUnexpectedConvertException.EXCEPTION;
+        }
+
+        return new ReportListItem(
+                report.getId(),
+                postId,
+                reportedContent,
+                report.getReporter().getName(),
+                report.getReported().getName(),
+                report.getReportType().getValue(),
+                report.getCreatedAt().toLocalDate()
+        );
+    }
+}
