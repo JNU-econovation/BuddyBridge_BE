8365616306bafef397e41c1aca989f68155f0672
Profile-exe <kang67572346@gmail.com>
2025-01-04 22:29:14 +0900

    feat(AdminReportController): 신고 API 관리자 기능은 개별 컨트롤러로 분리

---
+++ src/main/java/econo/buddybridge/report/controller/AdminReportController.java
@@ -0,0 +1,111 @@
+package econo.buddybridge.report.controller;
+
+import econo.buddybridge.auth.resolver.MemberTokenId;
+import econo.buddybridge.chat.chatmessage.dto.ChatMessageCustomPage;
+import econo.buddybridge.member.entity.Role;
+import econo.buddybridge.report.dto.ReportCustomPage;
+import econo.buddybridge.report.dto.ReportDetailResponse;
+import econo.buddybridge.report.service.ReportService;
+import econo.buddybridge.utils.api.ApiResponse;
+import econo.buddybridge.utils.api.ApiResponse.CustomBody;
+import econo.buddybridge.utils.api.ApiResponseGenerator;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/reports")
+@RequiredArgsConstructor
+@Tag(name = "관리자 API")
+public class AdminReportController {
+
+    private final ReportService reportService;
+
+    @Operation(summary = "신고 내역 상세 조회", description = "신고 내역을 상세 조회합니다.")
+    @GetMapping("/{report-id}")
+    public ApiResponse<CustomBody<ReportDetailResponse>> getReport(
+            @PathVariable("report-id") Long reportId,
+            @Parameter(hidden = true) @MemberTokenId(allowedRoles = {Role.ADMIN}) Long memberId
+    ) {
+        ReportDetailResponse report = reportService.getReport(reportId);
+        return ApiResponseGenerator.success(report, HttpStatus.OK);
+    }
+
+    @Operation(summary = "신고 내역 전체 조회", description = "신고 내역 전체를 조회합니다.")
+    @GetMapping
+    public ApiResponse<CustomBody<ReportCustomPage>> getReports(
+            @RequestParam("page") Integer page,
+            @RequestParam("size") Integer size,
+            @RequestParam(defaultValue = "desc", required = false) String sort,
+            @Parameter(hidden = true) @MemberTokenId(allowedRoles = {Role.ADMIN}) Long memberId
+    ) {
+        ReportCustomPage reports = reportService.getReports(page, size, sort);
+        return ApiResponseGenerator.success(reports, HttpStatus.OK);
+    }
+
+    @Operation(summary = "게시글 신고 내역 조회", description = "게시글 신고 내역을 조회합니다.")
+    @GetMapping("/posts")
+    public ApiResponse<CustomBody<ReportCustomPage>> getPostReports(
+            @RequestParam("page") Integer page,
+            @RequestParam("size") Integer size,
+            @RequestParam(defaultValue = "desc", required = false) String sort,
+            @Parameter(hidden = true) @MemberTokenId(allowedRoles = {Role.ADMIN}) Long memberId
+    ) {
+        ReportCustomPage reports = reportService.getPostReports(page, size, sort);
+        return ApiResponseGenerator.success(reports, HttpStatus.OK);
+    }
+
+    @Operation(summary = "댓글 신고 내역 조회", description = "댓글 신고 내역을 조회합니다.")
+    @GetMapping("/comments")
+    public ApiResponse<CustomBody<ReportCustomPage>> getCommentReports(
+            @RequestParam("page") Integer page,
+            @RequestParam("size") Integer size,
+            @RequestParam(defaultValue = "desc", required = false) String sort,
+            @Parameter(hidden = true) @MemberTokenId(allowedRoles = {Role.ADMIN}) Long memberId
+    ) {
+        ReportCustomPage reports = reportService.getCommentReports(page, size, sort);
+        return ApiResponseGenerator.success(reports, HttpStatus.OK);
+    }
+
+    @Operation(summary = "매칭 신고 내역 조회", description = "매칭 신고 내역을 조회합니다.")
+    @GetMapping("/matchings")
+    public ApiResponse<CustomBody<ReportCustomPage>> getMatchingReports(
+            @RequestParam("page") Integer page,
+            @RequestParam("size") Integer size,
+            @RequestParam(defaultValue = "desc", required = false) String sort,
+            @Parameter(hidden = true) @MemberTokenId(allowedRoles = {Role.ADMIN}) Long memberId
+    ) {
+        ReportCustomPage reports = reportService.getMatchingReports(page, size, sort);
+        return ApiResponseGenerator.success(reports, HttpStatus.OK);
+    }
+
+    @Operation(summary = "매칭 대화 내용 조회", description = "매칭 대화 내용을 조회합니다.")
+    @GetMapping("/{report-id}/chat")
+    public ApiResponse<CustomBody<ChatMessageCustomPage>> getChatMessages(
+            @PathVariable("report-id") Long reportId,
+            @RequestParam("limit") Integer size,
+            @RequestParam(value = "cursor", required = false) Long cursor,
+            @Parameter(hidden = true) @MemberTokenId(allowedRoles = {Role.ADMIN}) Long memberId
+    ) {
+        ChatMessageCustomPage chatMessages = reportService.getMatchingRoomMessages(reportId, size, cursor);
+        return ApiResponseGenerator.success(chatMessages, HttpStatus.OK);
+    }
+
+    @Operation(summary = "신고 내역 삭제", description = "신고 내역을 삭제합니다.")
+    @DeleteMapping("/{report-id}")
+    public ApiResponse<CustomBody<Void>> deleteReport(
+            @PathVariable("report-id") Long reportId,
+            @Parameter(hidden = true) @MemberTokenId(allowedRoles = {Role.ADMIN}) Long memberId
+    ) {
+        reportService.deleteReport(reportId);
+        return ApiResponseGenerator.success(HttpStatus.NO_CONTENT);
+    }
+}
--- src/main/java/econo/buddybridge/report/controller/ReportController.java
+++ src/main/java/econo/buddybridge/report/controller/ReportController.java
@@ -2,15 +2,11 @@ package econo.buddybridge.report.controller;
 
 import econo.buddybridge.auth.resolver.MemberTokenId;
 import econo.buddybridge.common.annotation.AllowAnonymous;
-import econo.buddybridge.member.entity.Role;
-import econo.buddybridge.report.dto.ReportCustomPage;
-import econo.buddybridge.report.dto.ReportDetailResponse;
 import econo.buddybridge.report.dto.ReportRequest;
 import econo.buddybridge.report.entity.ReportType;
 import econo.buddybridge.report.service.CommentReportService;
 import econo.buddybridge.report.service.MatchingReportService;
 import econo.buddybridge.report.service.PostReportService;
-import econo.buddybridge.report.service.ReportService;
 import econo.buddybridge.utils.api.ApiResponse;
 import econo.buddybridge.utils.api.ApiResponse.CustomBody;
 import econo.buddybridge.utils.api.ApiResponseGenerator;
@@ -22,13 +18,11 @@ import java.util.Arrays;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -37,7 +31,6 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "신고 API", description = "신고 관련 API")
 public class ReportController {
 
-    private final ReportService reportService;
     private final PostReportService postReportService;
     private final CommentReportService commentReportService;
     private final MatchingReportService matchingReportService;
@@ -50,64 +43,6 @@ public class ReportController {
                 .map(ReportType::getValue)
                 .toList();
         return ApiResponseGenerator.success(types, HttpStatus.OK);
-    }
-
-    @Operation(summary = "신고 내역 상세 조회", description = "신고 내역을 상세 조회합니다.")
-    @GetMapping("/{report-id}")
-    public ApiResponse<CustomBody<ReportDetailResponse>> getReport(
-            @PathVariable("report-id") Long reportId,
-            @Parameter(hidden = true) @MemberTokenId(allowedRoles = {Role.ADMIN}) Long memberId
-    ) {
-        ReportDetailResponse report = reportService.getReport(reportId);
-        return ApiResponseGenerator.success(report, HttpStatus.OK);
-    }
-
-    @Operation(summary = "신고 내역 전체 조회", description = "신고 내역 전체를 조회합니다.")
-    @GetMapping
-    public ApiResponse<CustomBody<ReportCustomPage>> getReports(
-            @RequestParam("page") Integer page,
-            @RequestParam("size") Integer size,
-            @RequestParam(defaultValue = "desc", required = false) String sort,
-            @Parameter(hidden = true) @MemberTokenId(allowedRoles = {Role.ADMIN}) Long memberId
-    ) {
-        ReportCustomPage reports = reportService.getReports(page, size, sort);
-        return ApiResponseGenerator.success(reports, HttpStatus.OK);
-    }
-
-    @Operation(summary = "게시글 신고 내역 조회", description = "게시글 신고 내역을 조회합니다.")
-    @GetMapping("/posts")
-    public ApiResponse<CustomBody<ReportCustomPage>> getPostReports(
-            @RequestParam("page") Integer page,
-            @RequestParam("size") Integer size,
-            @RequestParam(defaultValue = "desc", required = false) String sort,
-            @Parameter(hidden = true) @MemberTokenId(allowedRoles = {Role.ADMIN}) Long memberId
-    ) {
-        ReportCustomPage reports = reportService.getPostReports(page, size, sort);
-        return ApiResponseGenerator.success(reports, HttpStatus.OK);
-    }
-
-    @Operation(summary = "댓글 신고 내역 조회", description = "댓글 신고 내역을 조회합니다.")
-    @GetMapping("/comments")
-    public ApiResponse<CustomBody<ReportCustomPage>> getCommentReports(
-            @RequestParam("page") Integer page,
-            @RequestParam("size") Integer size,
-            @RequestParam(defaultValue = "desc", required = false) String sort,
-            @Parameter(hidden = true) @MemberTokenId(allowedRoles = {Role.ADMIN}) Long memberId
-    ) {
-        ReportCustomPage reports = reportService.getCommentReports(page, size, sort);
-        return ApiResponseGenerator.success(reports, HttpStatus.OK);
-    }
-
-    @Operation(summary = "매칭 신고 내역 조회", description = "매칭 신고 내역을 조회합니다.")
-    @GetMapping("/matchings")
-    public ApiResponse<CustomBody<ReportCustomPage>> getMatchingReports(
-            @RequestParam("page") Integer page,
-            @RequestParam("size") Integer size,
-            @RequestParam(defaultValue = "desc", required = false) String sort,
-            @Parameter(hidden = true) @MemberTokenId(allowedRoles = {Role.ADMIN}) Long memberId
-    ) {
-        ReportCustomPage reports = reportService.getMatchingReports(page, size, sort);
-        return ApiResponseGenerator.success(reports, HttpStatus.OK);
     }
 
     @Operation(summary = "게시글 신고", description = "게시글을 신고합니다.")
@@ -141,15 +76,5 @@ public class ReportController {
     ) {
         matchingReportService.reportMatching(matchingId, reportRequest, memberId);
         return ApiResponseGenerator.success(HttpStatus.CREATED);
-    }
-
-    @Operation(summary = "신고 내역 삭제", description = "신고 내역을 삭제합니다.")
-    @DeleteMapping("/{report-id}")
-    public ApiResponse<CustomBody<Void>> deleteReport(
-            @PathVariable("report-id") Long reportId,
-            @Parameter(hidden = true) @MemberTokenId(allowedRoles = Role.ADMIN) Long memberId
-    ) {
-        reportService.deleteReport(reportId);
-        return ApiResponseGenerator.success(HttpStatus.NO_CONTENT);
     }
 }
