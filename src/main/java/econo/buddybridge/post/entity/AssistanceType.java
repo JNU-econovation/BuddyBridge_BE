026189958f377663308a0825700e08191ff2623b
injae-348 <ant0593@naver.com>
2025-01-06 19:11:48 +0900

    refactor(AssistanceType): ValueEnum Interface 구현

--- src/main/java/econo/buddybridge/post/entity/AssistanceType.java
+++ src/main/java/econo/buddybridge/post/entity/AssistanceType.java
@@ -1,9 +1,11 @@
 package econo.buddybridge.post.entity;
 
+import econo.buddybridge.common.validation.ValueEnum;
+import java.util.Arrays;
 import lombok.Getter;
 
 @Getter
-public enum AssistanceType {
+public enum AssistanceType implements ValueEnum<String> {
     학습("학습"),
     식사("식사"),
     이동("이동"),
@@ -13,5 +15,17 @@ public enum AssistanceType {
 
     AssistanceType(String assistanceType) {
         this.assistanceType = assistanceType;
+    }
+
+    @Override
+    public String getValue() {
+        return assistanceType;
+    }
+
+    public static AssistanceType fromValue(String value) {
+        return Arrays.stream(AssistanceType.values())
+                .filter(assistanceType -> assistanceType.getValue().equals(value))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("AssistanceType에 해당하는 값이 없습니다."));
     }
 }
