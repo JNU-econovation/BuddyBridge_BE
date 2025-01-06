2f9b484f3c20ff3e02ecf1f4fd99941800af91c3
injae-348 <ant0593@naver.com>
2025-01-06 18:35:15 +0900

    feat(VolunteerCertificationMapper): 인증 폼 Mapper class 추가

---
+++ src/main/java/econo/buddybridge/certification/mapper/VolunteerCertificationMapper.java
@@ -0,0 +1,18 @@
+package econo.buddybridge.certification.mapper;
+
+import econo.buddybridge.certification.dto.VolunteerCertificationRequest;
+import econo.buddybridge.certification.entity.Volunteer;
+import econo.buddybridge.certification.entity.VolunteerTime;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public final class VolunteerCertificationMapper {
+
+    public static Volunteer toVolunteer(VolunteerCertificationRequest request) {
+        return new Volunteer(request.giverName(), request.giverEmail());
+    }
+
+    public static VolunteerTime toVolunteerTime(VolunteerCertificationRequest request) {
+        return new VolunteerTime(request.volunteerDate(), request.startTime(), request.endTime());
+    }
+}
