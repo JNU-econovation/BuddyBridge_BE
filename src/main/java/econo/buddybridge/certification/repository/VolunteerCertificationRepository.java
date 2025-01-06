fe36b7f123a8d291a020d5d6ede24d6c1f8609ed
injae-348 <ant0593@naver.com>
2025-01-06 18:32:34 +0900

    feat(VolunteerCertificationRepository): 인증 폼 저장소 추가

---
+++ src/main/java/econo/buddybridge/certification/repository/VolunteerCertificationRepository.java
@@ -0,0 +1,8 @@
+package econo.buddybridge.certification.repository;
+
+import econo.buddybridge.certification.entity.VolunteerCertification;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VolunteerCertificationRepository extends JpaRepository<VolunteerCertification, Long> {
+
+}
