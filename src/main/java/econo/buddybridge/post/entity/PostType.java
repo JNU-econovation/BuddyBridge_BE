d92d02bd04bb3074a8aeaea6026d4b5ecaaac5e1
injae-348 <ant0593@naver.com>
2025-01-06 18:57:52 +0900

    refactor(PostType): ValueEnum Interface 구현

--- src/main/java/econo/buddybridge/post/entity/PostType.java
+++ src/main/java/econo/buddybridge/post/entity/PostType.java
@@ -1,15 +1,29 @@
 package econo.buddybridge.post.entity;
 
+import econo.buddybridge.common.validation.ValueEnum;
+import java.util.Arrays;
 import lombok.Getter;
 
 @Getter
-public enum PostType {
+public enum PostType implements ValueEnum<String> {
     TAKER("도와줄래요?"),
     GIVER("도와줄게요!");
 
-    private final String postType;
+    private final String value;
 
-    PostType(String postType) {
-        this.postType = postType;
+    PostType(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String getValue() {
+        return value;
+    }
+
+    public static PostType fromValue(String value) {
+        return Arrays.stream(PostType.values())
+                .filter(postType -> postType.getValue().equals(value))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("PostType에 해당하는 값이 없습니다."));
     }
 }
