fcc454dda5a0d67ec21571b3d235a280d277eb95
injae-348 <ant0593@naver.com>
2025-01-07 21:20:35 +0900

    refactor(VolunteerCertificationValidator): 커스텀 비즈니스 예외 적용

--- src/main/java/econo/buddybridge/certification/validator/VolunteerCertificationValidator.java
+++ src/main/java/econo/buddybridge/certification/validator/VolunteerCertificationValidator.java
@@ -1,6 +1,12 @@
 package econo.buddybridge.certification.validator;
 
 import econo.buddybridge.certification.dto.VolunteerCertificationRequest;
+import econo.buddybridge.certification.exception.VolunteerCertificationAssistanceTimeMismatchException;
+import econo.buddybridge.certification.exception.VolunteerCertificationAssistanceTypeMismatchException;
+import econo.buddybridge.certification.exception.VolunteerCertificationNotAllowedMatchingStatusException;
+import econo.buddybridge.certification.exception.VolunteerCertificationPostTypeMismatchException;
+import econo.buddybridge.certification.exception.VolunteerCertificationScheduleDateMismatchException;
+import econo.buddybridge.certification.exception.VolunteerCertificationVolunteerMismatchException;
 import econo.buddybridge.matching.entity.Matching;
 import econo.buddybridge.matching.entity.MatchingStatus;
 import econo.buddybridge.member.entity.Member;
@@ -15,7 +21,7 @@ import org.springframework.stereotype.Component;
 public class VolunteerCertificationValidator {
 
     public void validateVolunteerCertification(Matching matching, Post post, Member member, VolunteerCertificationRequest request) {
-        // VOLUNTEERING_COMPLETED, VOLUNTEERING_VERIFIED 상태 확인
+        // VOLUNTEERING_COMPLETED, VOLUNTEERING_VERIFIED 상태 확인 Todo: 필요한지 고려
         validateMatchingStatus(matching);
 
         // 봉사자 정보 확인 (이름과 이메일을 검증했는데, 봉사자 = 매칭의 giver 이면됨)
@@ -37,19 +43,19 @@ public class VolunteerCertificationValidator {
     private void validateMatchingStatus(Matching matching) {
         if (!matching.getMatchingStatus().equals(MatchingStatus.VOLUNTEERING_COMPLETED) &&
                 !matching.getMatchingStatus().equals(MatchingStatus.VOLUNTEERING_VERIFIED)) {
-            throw new IllegalArgumentException("봉사 인증 폼 작성 중 문제가 발생했습니다. 매칭 상태가 VC 또는 VV 상태가 아닙니다.");
+            throw VolunteerCertificationNotAllowedMatchingStatusException.EXCEPTION;
         }
     }
 
     private void validateVolunteer(Matching matching, Member member) {
         if (!matching.getGiver().equals(member)) {
-            throw new IllegalArgumentException("봉사 인증 폼 작성 중 문제가 발생했습니다. 봉사자가 일치하지 않습니다. 관리자에게 문의해주세요.");
+            throw VolunteerCertificationVolunteerMismatchException.EXCEPTION;
         }
     }
 
     private void validatePostType(Post post, PostType postType) {
         if (!post.getPostType().equals(postType)) {
-            throw new IllegalArgumentException("봉사 인증 폼 작성 중 문제가 발생했습니다. 게시글 유형이 일치하지 않습니다. 관리자에게 문의해주세요.");
+            throw VolunteerCertificationPostTypeMismatchException.EXCEPTION;
         }
     }
 
@@ -58,7 +64,7 @@ public class VolunteerCertificationValidator {
         LocalDate endDate = post.getSchedule().getEndDate().toLocalDate();
 
         if (volunteerDate.isBefore(startDate) || volunteerDate.isAfter(endDate)) {
-            throw new IllegalArgumentException("봉사 인증 폼 작성 중 문제가 발생했습니다. 봉사 일자가 게시글의 일자에 포함되지 않습니다.");
+            throw VolunteerCertificationScheduleDateMismatchException.EXCEPTION;
         }
     }
 
@@ -67,13 +73,13 @@ public class VolunteerCertificationValidator {
         LocalTime assistanceEndTime = post.getAssistanceTime().getAssistanceEndTime();
 
         if (startTime.isBefore(assistanceStartTime) || endTime.isAfter(assistanceEndTime)) {
-            throw new IllegalArgumentException("봉사 인증 폼 작성 중 문제가 발생했습니다. 입력하신 봉사 시간이 게시글의 시작 및 종료 시간에 포함되지 않습니다.");
+            throw VolunteerCertificationAssistanceTimeMismatchException.EXCEPTION;
         }
     }
 
     private void validateAssistanceType(Post post, AssistanceType assistanceType) {
         if (!post.getAssistanceType().equals(assistanceType)) {
-            throw new IllegalArgumentException("봉사 인증 폼 작성 중 문제가 발생했습니다. 도움 유형이 일치하지 않습니다.");
+            throw VolunteerCertificationAssistanceTypeMismatchException.EXCEPTION;
         }
     }
 }
