a4ae8df9b2d6b3a5f21fd45ab015d80ec9497c4d
injae-348 <ant0593@naver.com>
2025-01-07 21:31:40 +0900

    feat(VolunteerCertificationService): 인증 폼 작성 시 매칭 상태 변경 설정

--- src/main/java/econo/buddybridge/certification/service/VolunteerCertificationService.java
+++ src/main/java/econo/buddybridge/certification/service/VolunteerCertificationService.java
@@ -7,7 +7,9 @@ import econo.buddybridge.certification.repository.VolunteerCertificationReposito
 import econo.buddybridge.certification.validator.VolunteerCertificationValidator;
 import econo.buddybridge.matching.entity.Matching;
 import econo.buddybridge.matching.service.MatchingService;
+import econo.buddybridge.matching.state.MatchingStatusChangeEvent;
 import econo.buddybridge.member.entity.Member;
+import econo.buddybridge.member.entity.MemberRole;
 import econo.buddybridge.member.service.MemberService;
 import econo.buddybridge.post.entity.Post;
 import lombok.RequiredArgsConstructor;
@@ -30,6 +32,12 @@ public class VolunteerCertificationService {
         Post post = matching.getPost();
 
         volunteerCertificationValidator.validateVolunteerCertification(matching, post, member, request);
+
+        // Todo : 하드 코딩된 부분
+        // FE에서 요청을 받아서 변경하는게 맞는지, 서버에서 변경하는게 맞는지
+        // 현재는 FE에서 아래와 같이 요청 보내면 상태 변경이 되버림 -> FE에서 SUBMIT_VOLUNTEERING_VERIFICATION 요청 못 보내게 설정
+        // 봉사 인증 완료도 여러번 글을 쓸 수 있어야 하니깐 -> VC에서 인증폼을 작성하면 DONE으로 변경하는건 어떨까? - 그리고 재 작성 가능하게
+        matching.handleEvent(MatchingStatusChangeEvent.SUBMIT_VOLUNTEERING_VERIFICATION, MemberRole.GIVER);
 
         volunteerCertificationRepository.save(VolunteerCertification.of(
                 VolunteerCertificationMapper.toVolunteer(request),
