0383bde299b3e4f1638f03b66e5dbf0d2a3a85f8
injae-348 <ant0593@naver.com>
2025-01-08 02:21:11 +0900

    docs(VolunteeringCertificationService): 불필요한 주석 제거

--- src/main/java/econo/buddybridge/certification/service/VolunteerCertificationService.java
+++ src/main/java/econo/buddybridge/certification/service/VolunteerCertificationService.java
@@ -35,10 +35,6 @@ public class VolunteerCertificationService {
 
         volunteerCertificationValidator.validateVolunteerCertification(matching, post, member, request);
 
-        // Todo : 하드 코딩된 부분
-        // FE에서 요청을 받아서 변경하는게 맞는지, 서버에서 변경하는게 맞는지
-        // 현재는 FE에서 아래와 같이 요청 보내면 상태 변경이 되버림 -> FE에서 SUBMIT_VOLUNTEERING_VERIFICATION 요청 못 보내게 설정
-        // 봉사 인증 완료도 여러번 글을 쓸 수 있어야 하니깐 -> VC에서 인증폼을 작성하면 DONE으로 변경하는건 어떨까? - 그리고 재 작성 가능하게
         matching.handleEvent(MatchingStatusChangeEvent.SUBMIT_VOLUNTEERING_VERIFICATION, MemberRole.GIVER);
 
         volunteerCertificationRepository.save(VolunteerCertification.of(
