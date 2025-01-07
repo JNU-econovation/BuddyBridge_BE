87118d0a360f7924f4f8001ffa432d56d5940e03
injae-348 <ant0593@naver.com>
2025-01-07 23:53:12 +0900

    feat(VolunteerCertificationRepository): Fetch Join으로 매칭 조회해서 인증 폼 반환 함수 추가

--- src/main/java/econo/buddybridge/certification/repository/VolunteerCertificationRepository.java
+++ src/main/java/econo/buddybridge/certification/repository/VolunteerCertificationRepository.java
@@ -1,8 +1,13 @@
 package econo.buddybridge.certification.repository;
 
 import econo.buddybridge.certification.entity.VolunteerCertification;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface VolunteerCertificationRepository extends JpaRepository<VolunteerCertification, Long> {
 
+    @Query("SELECT vc FROM VolunteerCertification vc JOIN FETCH vc.matching WHERE vc.id = :volunteerCertificationId")
+    Optional<VolunteerCertification> findByIdWithMatching(@Param("volunteerCertificationId") Long volunteerCertificationId);
 }
