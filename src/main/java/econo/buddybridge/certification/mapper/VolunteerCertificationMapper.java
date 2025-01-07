b460a60b563aca0b95022d547c9dfb6375e8e47a
injae-348 <ant0593@naver.com>
2025-01-08 00:01:07 +0900

    feat(VolunteerCertificationMapper): 업데이트 요청 DTO에 대해 VolunteerTime 생성 메서드 추가

--- src/main/java/econo/buddybridge/certification/mapper/VolunteerCertificationMapper.java
+++ src/main/java/econo/buddybridge/certification/mapper/VolunteerCertificationMapper.java
@@ -1,6 +1,7 @@
 package econo.buddybridge.certification.mapper;
 
 import econo.buddybridge.certification.dto.VolunteerCertificationRequest;
+import econo.buddybridge.certification.dto.VolunteerCertificationUpdateRequest;
 import econo.buddybridge.certification.entity.Volunteer;
 import econo.buddybridge.certification.entity.VolunteerTime;
 import lombok.experimental.UtilityClass;
@@ -13,6 +14,10 @@ public final class VolunteerCertificationMapper {
     }
 
     public static VolunteerTime toVolunteerTime(VolunteerCertificationRequest request) {
+        return new VolunteerTime(request.volunteerDate(), request.startTime(), request.endTime());
+    }
+
+    public static VolunteerTime toVolunteerTime(VolunteerCertificationUpdateRequest request) {
         return new VolunteerTime(request.volunteerDate(), request.startTime(), request.endTime());
     }
 }
