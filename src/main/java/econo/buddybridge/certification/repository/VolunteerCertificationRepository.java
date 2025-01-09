d5a746b8f9074a6d5c0d416ed3f75d64b1ebac6f
injae-348 <ant0593@naver.com>
2025-01-09 21:50:13 +0900

    feat(VolunteerCertificationRepository): 사용자가 작성한 인증 폼 상세 조회 반환 DTO 추가, CertificationDetailQueryDto 조회시 id 대신 Entity 사용

--- src/main/java/econo/buddybridge/certification/repository/VolunteerCertificationRepository.java
+++ src/main/java/econo/buddybridge/certification/repository/VolunteerCertificationRepository.java
@@ -1,13 +1,37 @@
 package econo.buddybridge.certification.repository;
 
 import econo.buddybridge.certification.dto.detail.CertificationDetailQueryDto;
+import econo.buddybridge.certification.dto.detail.VolunteeringDetailResponse;
 import econo.buddybridge.certification.entity.VolunteerCertification;
+import econo.buddybridge.member.entity.Member;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface VolunteerCertificationRepository extends JpaRepository<VolunteerCertification, Long>, VolunteerCertificationCustomRepository {
+
+    @Query(
+            "SELECT new econo.buddybridge.certification.dto.detail.VolunteeringDetailResponse(" +
+                    "   vc.id, " +
+                    "   m.giver.nickname, " +
+                    "   vc.createdAt, " +
+                    "   vc.volunteerInfo.name, " +
+                    "   vc.volunteerInfo.email, " +
+                    "   p.id, " +
+                    "   p.postType, " +
+                    "   vc.volunteerTime.volunteerDate, " +
+                    "   p.assistanceType, " +
+                    "   vc.volunteerTime.startTime, " +
+                    "   vc.volunteerTime.endTime, " +
+                    "   vc.content" +
+                    ") " +
+                    "FROM VolunteerCertification vc " +
+                    "JOIN vc.matching m " +
+                    "JOIN m.post p " +
+                    "WHERE vc.matching.giver = :member AND vc = :volunteerCertification"
+    )
+    VolunteeringDetailResponse findVolunteeringDetailResponseByMemberAndCertification(@Param("member") Member member, @Param("volunteerCertification") VolunteerCertification volunteerCertification);
 
     @Query("SELECT EXISTS (SELECT 1 FROM VolunteerCertification vc WHERE vc.matching.id = :matchingId)")
     boolean existsByMatchingId(Long matchingId);
@@ -45,9 +69,9 @@ public interface VolunteerCertificationRepository extends JpaRepository<Voluntee
                     "LEFT JOIN vc.matching m " +
                     "LEFT JOIN m.post p " +
                     "LEFT JOIN p.author a " +
-                    "WHERE vc.id = :volunteerCertificationId"
+                    "WHERE vc = :volunteerCertification"
     )
-    CertificationDetailQueryDto findCertificationDetailQueryDtoById(@Param("volunteerCertificationId") Long volunteerCertificationId);
+    CertificationDetailQueryDto findCertificationDetailQueryDtoByVolunteerCertification(@Param("volunteerCertification") VolunteerCertification volunteerCertification);
 
     @Query("SELECT vc FROM VolunteerCertification vc JOIN FETCH vc.matching WHERE vc.id = :volunteerCertificationId")
     Optional<VolunteerCertification> findByIdWithMatching(@Param("volunteerCertificationId") Long volunteerCertificationId);
