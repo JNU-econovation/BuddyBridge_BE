067e8cd4959f840c1b61ad32062a04427f6f3a87
injae-348 <ant0593@naver.com>
2025-01-09 16:47:35 +0900

    feat(VolunteerCertificationRepository): 관리자 페이지에서 인증 폼 상세 정보 조회시 필요한 값 반환 JPQL 추가

--- src/main/java/econo/buddybridge/certification/repository/VolunteerCertificationRepository.java
+++ src/main/java/econo/buddybridge/certification/repository/VolunteerCertificationRepository.java
@@ -1,5 +1,6 @@
 package econo.buddybridge.certification.repository;
 
+import econo.buddybridge.certification.dto.detail.CertificationDetailQueryDto;
 import econo.buddybridge.certification.entity.VolunteerCertification;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,6 +9,43 @@ import org.springframework.data.repository.query.Param;
 
 public interface VolunteerCertificationRepository extends JpaRepository<VolunteerCertification, Long>, VolunteerCertificationCustomRepository {
 
+    @Query(
+            "SELECT new econo.buddybridge.certification.dto.detail.CertificationDetailQueryDto(" +
+                    "   a.name, " +
+                    "   a.nickname, " +
+                    "   a.gender, " +
+                    "   a.age, " +
+                    "   a.disabilityType, " +
+                    "   p.title, " +
+                    "   p.district, " +
+                    "   p.schedule.startDate, " +
+                    "   p.schedule.endDate, " +
+                    "   p.schedule.scheduleType, " +
+                    "   p.schedule.scheduleDetails, " +
+                    "   p.assistanceTime.assistanceStartTime, " +
+                    "   p.assistanceTime.assistanceEndTime, " +
+                    "   p.content, " +
+                    "   vc.id, " +
+                    "   m.giver.nickname, " +
+                    "   vc.createdAt, " +
+                    "   vc.volunteerInfo.name, " +
+                    "   vc.volunteerInfo.email, " +
+                    "   p.id, " +
+                    "   p.postType, " +
+                    "   vc.volunteerTime.volunteerDate, " +
+                    "   p.assistanceType, " +
+                    "   vc.volunteerTime.startTime, " +
+                    "   vc.volunteerTime.endTime, " +
+                    "   vc.content" +
+                    ") " +
+                    "FROM VolunteerCertification vc " +
+                    "LEFT JOIN vc.matching m " +
+                    "LEFT JOIN m.post p " +
+                    "LEFT JOIN p.author a " +
+                    "WHERE vc.id = :volunteerCertificationId"
+    )
+    CertificationDetailQueryDto findCertificationDetailQueryDtoById(@Param("volunteerCertificationId") Long volunteerCertificationId);
+    
     @Query("SELECT vc FROM VolunteerCertification vc JOIN FETCH vc.matching WHERE vc.id = :volunteerCertificationId")
     Optional<VolunteerCertification> findByIdWithMatching(@Param("volunteerCertificationId") Long volunteerCertificationId);
 }
