4aa244215fc27f33d5fcb939bbabb2fb89375f19
Profile-exe <kang67572346@gmail.com>
2025-01-11 00:03:42 +0900

    feat: 댓글 및 매칭 신고에 대한 게시글 조회 메서드 구현

--- src/main/java/econo/buddybridge/report/repository/PostReportRepository.java
+++ src/main/java/econo/buddybridge/report/repository/PostReportRepository.java
@@ -4,6 +4,7 @@ import econo.buddybridge.member.entity.Member;
 import econo.buddybridge.post.entity.Post;
 import econo.buddybridge.report.entity.PostReport;
 import java.util.List;
+import java.util.Set;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -12,5 +13,5 @@ public interface PostReportRepository extends JpaRepository<PostReport, Long> {
     boolean existsByReportedPostAndReporter(Post reportedPost, Member reporter);
 
     @Query("SELECT pr.reportedPost FROM PostReport pr WHERE pr.reportedPost IN :reportedPosts")
-    List<Post> findByReportedPostIn(List<Post> reportedPosts);
+    Set<Post> findByReportedPostIn(List<Post> reportedPosts);
 }
