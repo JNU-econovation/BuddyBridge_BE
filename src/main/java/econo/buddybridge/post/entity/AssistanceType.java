76618b9e3299ec8aba62e1a869cfe6954d5643ec
injae-348 <ant0593@naver.com>
2025-01-11 11:19:31 +0900

    refactor(post.entity.enum): AssistanceType, PostType에 ValueEnum 인터페이스 적용
    - assistanceType, postType 변수명 value로 변경
    - PostType 열거형 인스턴스 필드 한글에서 영어로 변경

--- src/main/java/econo/buddybridge/post/entity/AssistanceType.java
+++ src/main/java/econo/buddybridge/post/entity/AssistanceType.java
@@ -1,17 +1,32 @@
 package econo.buddybridge.post.entity;
 
+import econo.buddybridge.common.validation.ValueEnum;
+import econo.buddybridge.post.exception.AssistanceTypeInvalidException;
+import java.util.Arrays;
 import lombok.Getter;
 
 @Getter
-public enum AssistanceType {
+public enum AssistanceType implements ValueEnum<String> {
     학습("학습"),
     식사("식사"),
     이동("이동"),
     기타("기타");
 
-    private final String assistanceType;
+    private final String value;
 
-    AssistanceType(String assistanceType) {
-        this.assistanceType = assistanceType;
+    AssistanceType(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String getValue() {
+        return value;
+    }
+
+    public static AssistanceType fromValue(String value) {
+        return Arrays.stream(AssistanceType.values())
+                .filter(assistanceType -> assistanceType.getValue().equals(value))
+                .findFirst()
+                .orElseThrow(() -> AssistanceTypeInvalidException.EXCEPTION);
     }
 }
