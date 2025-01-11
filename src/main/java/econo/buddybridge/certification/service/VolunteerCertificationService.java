279a6d088793142217d03077a370126fdf456fb4
injae-348 <ant0593@naver.com>
2025-01-11 13:46:29 +0900

    feat(VolunteerCertificationService): 봉사활동 인증 폼 생성 서비스 구현

---
+++ src/main/java/econo/buddybridge/certification/service/VolunteerCertificationService.java
@@ -0,0 +1,46 @@
+package econo.buddybridge.certification.service;
+
+import econo.buddybridge.certification.dto.VolunteerCertificationRequest;
+import econo.buddybridge.certification.entity.VolunteerCertification;
+import econo.buddybridge.certification.mapper.VolunteerCertificationMapper;
+import econo.buddybridge.certification.repository.VolunteerCertificationRepository;
+import econo.buddybridge.certification.validator.VolunteerCertificationValidator;
+import econo.buddybridge.matching.entity.Matching;
+import econo.buddybridge.matching.service.MatchingService;
+import econo.buddybridge.matching.state.MatchingStatusChangeEvent;
+import econo.buddybridge.member.entity.Member;
+import econo.buddybridge.member.entity.MemberRole;
+import econo.buddybridge.member.service.MemberService;
+import econo.buddybridge.post.entity.Post;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class VolunteerCertificationService {
+
+    private final VolunteerCertificationRepository volunteerCertificationRepository;
+    private final MatchingService matchingService;
+    private final MemberService memberService;
+    private final VolunteerCertificationValidator volunteerCertificationValidator;
+
+    @Transactional
+    public void submitVolunteerCertification(Long matchingId, VolunteerCertificationRequest request, Long memberId) {
+        Member member = memberService.findMemberByIdOrThrow(memberId);
+        Matching matching = matchingService.findByIdWithMembersAndPost(matchingId);
+        Post post = matching.getPost();
+
+        volunteerCertificationValidator.validateVolunteerCertification(matching, post, member, request);
+
+        // GIVER가 인증 폼 작성을 완료하면 VOLUNTEERING_VERIFICATION 상태로 변경, 하드 코딩..
+        matching.handleEvent(MatchingStatusChangeEvent.SUBMIT_VOLUNTEERING_VERIFICATION, MemberRole.GIVER);
+        
+        volunteerCertificationRepository.save(VolunteerCertification.of(
+                VolunteerCertificationMapper.toVolunteerer(request),
+                matching,
+                VolunteerCertificationMapper.toVolunteerTime(request),
+                request.content()
+        ));
+    }
+}
