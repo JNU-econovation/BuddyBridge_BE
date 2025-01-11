18426fd441576630f48745237d5483162657846c
injae-348 <ant0593@naver.com>
2025-01-11 11:51:16 +0900

    feat(VolunteerCertificationRequest): 봉사 인증 폼 생성을 위한 Request DTO 추가
    - Annotation 기반 검증
    - @AssertTrue를 통해 시작, 종료 시간 검증

---
+++ src/main/java/econo/buddybridge/certification/dto/VolunteerCertificationRequest.java
@@ -0,0 +1,44 @@
+package econo.buddybridge.certification.dto;
+
+import econo.buddybridge.common.validation.EnumTypeValue;
+import econo.buddybridge.post.entity.AssistanceType;
+import econo.buddybridge.post.entity.PostType;
+import jakarta.validation.constraints.AssertTrue;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public record VolunteerCertificationRequest(
+        @NotBlank(message = "봉사자 이름은 필수 입니다.")
+        String volunteerName,
+
+        @NotBlank(message = "봉사자 이메일은 필수 입니다.")
+        String volunteerEmail,
+
+        @EnumTypeValue(enumClass = PostType.class)
+        String postType,
+
+        @NotNull(message = "봉사 일자는 필수 입니다.")
+        LocalDate volunteerDate,
+
+        @EnumTypeValue(enumClass = AssistanceType.class)
+        String assistanceType,
+
+        @NotNull(message = "봉사 시작 시간은 필수 입니다.")
+        LocalTime startTime,
+
+        @NotNull(message = "봉사 종료 시간은 필수 입니다.")
+        LocalTime endTime,
+
+        @NotBlank(message = "봉사 내용은 필수 입니다.")
+        @Size(min = 200, max = 1000, message = "봉사 활동 내용 및 소감은 200자 이상 1000자 이하로 작성해주세요.")
+        String content
+) {
+
+    @AssertTrue(message = "봉사 시작 시간은 봉사 종료 시간보다 빨라야 합니다.")
+    private boolean isStartTimeBeforeEndTime() {
+        return !startTime.isAfter(endTime);
+    }
+}
