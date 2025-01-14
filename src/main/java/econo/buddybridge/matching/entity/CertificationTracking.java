14a67ad7e1195a91934573b92c07063ed5c96d43
injae-348 <ant0593@naver.com>
2025-01-14 20:56:49 +0900

    refactor(Matching): CertificationTracking 양방향에서 단방향 관계 변경
    - Matching에서 CertificationTracking을 생성 관리 하므로 단방향 설정
    - 매칭이 완료된 후 3일이 지나야 "봉사 인증 완료" 버튼을 클릭 할 수
      있으므로 certificationTracking의 requestedAt에 2일을 더해 생성

--- src/main/java/econo/buddybridge/matching/entity/CertificationTracking.java
+++ src/main/java/econo/buddybridge/matching/entity/CertificationTracking.java
@@ -3,11 +3,9 @@ package econo.buddybridge.matching.entity;
 import econo.buddybridge.matching.exception.certification.RequestCoolDownPeriodException;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
 import lombok.AccessLevel;
@@ -26,20 +24,15 @@ public class CertificationTracking {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @OneToOne(fetch = FetchType.LAZY)
-    private Matching matching;
-
     private LocalDateTime requestedAt;
 
     @Builder
-    private CertificationTracking(Matching matching, LocalDateTime requestedAt) {
-        this.matching = matching;
+    private CertificationTracking(LocalDateTime requestedAt) {
         this.requestedAt = requestedAt;
     }
 
-    public static CertificationTracking of(Matching matching, LocalDateTime requestedAt) {
+    public static CertificationTracking of(LocalDateTime requestedAt) {
         return CertificationTracking.builder()
-                .matching(matching)
                 .requestedAt(requestedAt)
                 .build();
     }
--- src/main/java/econo/buddybridge/matching/entity/Matching.java
+++ src/main/java/econo/buddybridge/matching/entity/Matching.java
@@ -30,7 +30,6 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.PostLoad;
-import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
 import java.time.LocalDateTime;
@@ -83,14 +82,8 @@ public class Matching extends SoftDeletableEntity {
         this.taker = taker;
         this.giver = giver;
         this.matchingStatus = matchingStatus;
+        this.certificationTracking = CertificationTracking.of(LocalDateTime.now().plusDays(2));
         initializeMatchingState();
-    }
-
-    private static final LocalDateTime INITIAL_REQUESTED_AT = LocalDateTime.of(2000, 1, 1, 0, 0, 0);
-
-    @PrePersist
-    private void prePersist() {
-        this.certificationTracking = CertificationTracking.of(this, INITIAL_REQUESTED_AT);
     }
 
     public boolean canRequestCertification() {
