68f677f90d71741404a014dcbbcddc4372699883
Profile-exe <kang67572346@gmail.com>
2025-01-17 00:52:48 +0900

    feat(NotificationRepository): 회원의 알림 삭제 쿼리

--- src/main/java/econo/buddybridge/notification/repository/NotificationRepository.java
+++ src/main/java/econo/buddybridge/notification/repository/NotificationRepository.java
@@ -1,6 +1,8 @@
 package econo.buddybridge.notification.repository;
 
+import econo.buddybridge.member.entity.Member;
 import econo.buddybridge.notification.entity.Notification;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -15,4 +17,8 @@ public interface NotificationRepository extends JpaRepository<Notification, Long
     @Modifying(flushAutomatically = true, clearAutomatically = true)
     @Query("update Notification n set n.isRead = true where n.receiver.id = :memberId and n.url = :url")
     void markAsReadByMemberIdAndUrl(@Param("memberId") Long memberId, @Param("url") String url);
+
+    @Modifying(flushAutomatically = true, clearAutomatically = true)
+    @Query("DELETE FROM Notification n WHERE n.receiver IN :members")
+    void deleteAllByMemberIn(List<Member> members);
 }
