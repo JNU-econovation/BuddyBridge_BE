5ac2719ac62ea45698090e19f46fe63fe919ba87
Profile-exe <kang67572346@gmail.com>
2025-01-17 00:54:44 +0900

    feat(MemberRepository): 회원 조회 및 삭제 쿼리

--- src/main/java/econo/buddybridge/member/repository/MemberRepository.java
+++ src/main/java/econo/buddybridge/member/repository/MemberRepository.java
@@ -1,8 +1,11 @@
 package econo.buddybridge.member.repository;
 
 import econo.buddybridge.member.entity.Member;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom {
 
@@ -13,4 +16,10 @@ public interface MemberRepository extends JpaRepository<Member, Long>, MemberRep
     boolean existsByEmail(String email);
 
     Optional<Member> findByEmail(String email);
+
+    List<Member> findByIdIn(List<Long> memberIds);
+
+    @Modifying(flushAutomatically = true, clearAutomatically = true)
+    @Query("DELETE FROM Member m WHERE m IN :members")
+    void deleteAllIn(List<Member> members);
 }
