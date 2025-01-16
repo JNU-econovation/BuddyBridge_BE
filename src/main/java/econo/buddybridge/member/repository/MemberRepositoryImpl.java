d494a40dd6a89756fb418b3eb964cdba427c309e
Profile-exe <kang67572346@gmail.com>
2025-01-16 21:57:02 +0900

    feat: 회원 목록 조회를 위한 쿼리 구현

---
+++ src/main/java/econo/buddybridge/member/repository/MemberRepositoryImpl.java
@@ -0,0 +1,48 @@
+package econo.buddybridge.member.repository;
+
+import static econo.buddybridge.member.entity.QMember.member;
+import static econo.buddybridge.report.entity.QReport.report;
+
+import com.querydsl.jpa.JPAExpressions;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import econo.buddybridge.member.dto.MemberListItem;
+import econo.buddybridge.member.dto.QMemberListItem;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class MemberRepositoryImpl implements MemberRepositoryCustom {
+
+    private final JPAQueryFactory queryFactory;
+
+    @Override
+    public List<MemberListItem> findMembers(Integer page, Integer size, String sort) {
+        return queryFactory
+                .select(new QMemberListItem(
+                        member.id,
+                        member.nickname,
+                        member.email,
+                        member.gender,
+                        member.age,
+                        member.disabilityType,
+                        member.email,
+                        JPAExpressions
+                                .select(report.count())
+                                .from(report)
+                                .where(report.reported.eq(member))
+                ))
+                .from(member)
+                .offset((long) page * size)
+                .limit(size)
+                .orderBy(member.createdAt.desc())
+                .fetch();
+    }
+
+    @Override
+    public Long totalElements() {
+        return queryFactory
+                .select(member.count())
+                .from(member)
+                .fetchOne();
+    }
+}
