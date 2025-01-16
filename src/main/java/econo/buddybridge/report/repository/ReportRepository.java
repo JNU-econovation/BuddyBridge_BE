b3abf2513d4005de44eb6d387a205a17d1c6443a
Profile-exe <kang67572346@gmail.com>
2025-01-17 00:53:21 +0900

    feat(ReportRepository): 회원의 신고내역 및 피신고내역 삭제 쿼리

--- src/main/java/econo/buddybridge/report/repository/ReportRepository.java
+++ src/main/java/econo/buddybridge/report/repository/ReportRepository.java
@@ -1,8 +1,15 @@
 package econo.buddybridge.report.repository;
 
+import econo.buddybridge.member.entity.Member;
 import econo.buddybridge.report.entity.Report;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ReportRepository extends JpaRepository<Report, Long>, ReportRepositoryCustom {
 
+    @Modifying(flushAutomatically = true, clearAutomatically = true)
+    @Query("DELETE FROM Report r WHERE r.reporter IN :members OR r.reported IN :members")
+    void deleteAllByMemberIn(List<Member> members);
 }
