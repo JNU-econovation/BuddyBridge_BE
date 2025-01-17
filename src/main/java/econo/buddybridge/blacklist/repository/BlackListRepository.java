4daa5f940f844eb605042c8741bd0bcf9bfa505d
injae-348 <ant0593@naver.com>
2025-01-18 00:25:37 +0900

    feat(BlackListRepository): 신고된 회원 관련 Repository 메서드 추가
    - deleteByReportedMember: 신고된 회원 정보로 블랙리스트 삭제
    - existsByReportedMember: 신고된 회원의 블랙리스트 등록 여부 확인

---
+++ src/main/java/econo/buddybridge/blacklist/repository/BlackListRepository.java
@@ -0,0 +1,12 @@
+package econo.buddybridge.blacklist.repository;
+
+import econo.buddybridge.blacklist.entity.BlackList;
+import econo.buddybridge.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BlackListRepository extends JpaRepository<BlackList, Long> {
+
+    void deleteByReportedMember(Member reportedMember);
+
+    boolean existsByReportedMember(Member reportedMember);
+}
