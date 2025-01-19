150f4f29a3c89f86a4494a5a7d004df366430de9
injae-348 <ant0593@naver.com>
2025-01-19 13:03:22 +0900

    refactor(PostRepositoryImpl): PostRepositoryCustomImpl로 이름 변경

--- src/main/java/econo/buddybridge/post/repository/PostRepositoryCustomImpl.java
+++ src/main/java/econo/buddybridge/post/repository/PostRepositoryCustomImpl.java
@@ -32,7 +32,7 @@ import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-public class PostRepositoryImpl implements PostRepositoryCustom {
+public class PostRepositoryCustomImpl implements PostRepositoryCustom {
 
     private final JPAQueryFactory queryFactory;
 
