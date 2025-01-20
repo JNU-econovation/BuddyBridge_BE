34879264b5be24d8ed086e9e92a581e119013437
Profile-exe <kang67572346@gmail.com>
2025-01-20 13:09:17 +0900

    refactor(PostDeleteEventHandler): 게시글 삭제 로직 함수 분리

--- src/main/java/econo/buddybridge/post/event/handler/PostDeleteEventHandler.java
+++ src/main/java/econo/buddybridge/post/event/handler/PostDeleteEventHandler.java
@@ -37,36 +37,50 @@ public class PostDeleteEventHandler {
     @TransactionalEventListener(phase = TransactionPhase.BEFORE_COMMIT)
     public void handlePostDeleteEvent(PostDeleteEvent event) {
         List<Post> posts = event.getPosts();
+        Set<Post> reportedPosts = getReportedPosts(posts);
 
+        // 신고된 게시글은 soft delete
+        if (!reportedPosts.isEmpty()) {
+            softDeleteAboutPost(reportedPosts);
+        }
+
+        posts = posts.stream()  // 신고된 게시글이 아닌 경우만 필터링
+                .filter(post -> !reportedPosts.contains(post))
+                .toList();
+
+        // 신고된 게시글이 아닌 경우 완전 삭제
+        if (!posts.isEmpty()) {
+            hardDeleteAboutMatching(posts);
+            hardDeleteAboutPost(posts);
+        }
+    }
+
+    private Set<Post> getReportedPosts(List<Post> posts) {
         Set<Post> reportedPosts = postReportRepository.findByReportedPostIn(posts);
         Set<Post> reportedCommentPosts = commentReportRepository.findPostByReportedCommentPostIn(posts);
         Set<Post> reportedMatchingPosts = matchingReportRepository.findPostByReportedMatchingPostIn(posts);
 
         reportedPosts.addAll(reportedCommentPosts);
         reportedPosts.addAll(reportedMatchingPosts);
+        return reportedPosts;
+    }
 
-        // 신고된 게시글은 soft delete
-        if (!reportedPosts.isEmpty()) {
-            postRepository.softDeleteAllIn(reportedPosts);
-            commentRepository.softDeleteAllByPostIn(reportedPosts);
-            matchingRepository.softDeleteAllByPostIn(reportedPosts);
-        }
+    private void softDeleteAboutPost(Set<Post> reportedPosts) {
+        postRepository.softDeleteAllIn(reportedPosts);
+        commentRepository.softDeleteAllByPostIn(reportedPosts);
+        matchingRepository.softDeleteAllByPostIn(reportedPosts);
+    }
 
-        // 신고된 게시글이 아닌 경우 완전 삭제
-        posts = posts.stream()
-                .filter(post -> !reportedPosts.contains(post))
-                .toList();
+    private void hardDeleteAboutMatching(List<Post> posts) {
+        List<Matching> matchings = matchingRepository.findByPostIn(posts);
+        chatMessageRepository.deleteAllByMatchingIn(matchings);
+        messageReadStatusRepository.deleteAllByMatchingIn(matchings);
+        matchingRepository.deleteAllInBatch(matchings);
+    }
 
-        if (!posts.isEmpty()) {
-            List<Matching> matchings = matchingRepository.findByPostIn(posts);
-            chatMessageRepository.deleteAllByMatchingIn(matchings);
-            messageReadStatusRepository.deleteAllByMatchingIn(matchings);
-            matchingRepository.deleteAllInBatch(matchings);
-
-            commentRepository.deleteAllByPostIn(posts);
-            postLikeRepository.deleteAllByPostIn(posts);
-
-            postRepository.deleteAllIn(posts);
-        }
+    private void hardDeleteAboutPost(List<Post> posts) {
+        commentRepository.deleteAllByPostIn(posts);
+        postLikeRepository.deleteAllByPostIn(posts);
+        postRepository.deleteAllIn(posts);
     }
 }
