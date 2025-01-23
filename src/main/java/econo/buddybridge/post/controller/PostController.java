469f146f798d6913cdac3e5311de9581db9bf584
injae-348 <ant0593@naver.com>
2025-01-24 00:53:46 +0900

    refactor(PostController): 게시글 업데이트 요청 Patch에서 Put으로 변경

--- src/main/java/econo/buddybridge/post/controller/PostController.java
+++ src/main/java/econo/buddybridge/post/controller/PostController.java
@@ -24,9 +24,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -86,7 +86,7 @@ public class PostController {
     }
 
     @Operation(summary = "게시글 수정", description = "게시글을 수정합니다.")
-    @PatchMapping("/{post-id}")
+    @PutMapping("/{post-id}")
     public ApiResponse<ApiResponse.CustomBody<Long>> updatePost(
             @PathVariable("post-id") Long postId,
             @Valid @RequestBody PostUpdateReqDto postUpdateReqDto,
