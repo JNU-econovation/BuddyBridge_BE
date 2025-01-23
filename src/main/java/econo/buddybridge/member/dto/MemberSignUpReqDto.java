9fed797abcfe97f88e19e7e58f90f723c414a38a
injae-348 <ant0593@naver.com>
2025-01-23 17:08:21 +0900

    refactor: 회원가입 및 정보 수정 시 정규표현식 적용
    - MemberSignUpReqDto: 비밀번호, 이름, 닉네임 정규표현식 적용
    - NicknameReqDto: 닉네임 정규표현식 적용

--- src/main/java/econo/buddybridge/member/dto/MemberSignUpReqDto.java
+++ src/main/java/econo/buddybridge/member/dto/MemberSignUpReqDto.java
@@ -4,14 +4,16 @@ import econo.buddybridge.member.entity.Gender;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-
+import jakarta.validation.constraints.Pattern;
 import java.time.LocalDate;
 
 public record MemberSignUpReqDto(
         @NotBlank(message = "이름을 입력해주세요.")
+        @Pattern(regexp = "^[a-zA-Z가-힣\\s]{2,18}$", message = "이름은 2-18자의 영문, 한글, 공백만 입력 가능합니다.")
         String name,
 
         @NotBlank(message = "닉네임을 입력해주세요.")
+        @Pattern(regexp = "^[a-zA-Z0-9가-힣\\s]{2,10}$", message = "닉네임은 2-10자의 영문, 숫자, 한글, 공백만 입력 가능합니다.")
         String nickname,
 
         @NotNull(message = "성별을 선택해주세요.")
@@ -24,7 +26,7 @@ public record MemberSignUpReqDto(
         @Email(message = "이메일 형식이 올바르지 않습니다.")
         String email,
 
-        @NotBlank(message = "비밀번호를 입력해주세요.")
+        @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[!@#$%^&*(),.?\":{}|<>])(?=\\S+$).{8,16}$", message = "비밀번호는 영문, 숫자, 특수문자를 포함하여 8자 이상 16자 이하로 입력해주세요.")
         String password
 ) {
 
--- src/main/java/econo/buddybridge/member/dto/NicknameReqDto.java
+++ src/main/java/econo/buddybridge/member/dto/NicknameReqDto.java
@@ -1,9 +1,12 @@
 package econo.buddybridge.member.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 public record NicknameReqDto(
         @NotBlank(message = "닉네임을 입력해주세요.")
+        @Pattern(regexp = "^[a-zA-Z0-9가-힣\\s]{2,10}$", message = "닉네임은 2-10자의 영문, 숫자, 한글, 공백만 입력 가능합니다.")
         String nickname
 ) {
+
 }
