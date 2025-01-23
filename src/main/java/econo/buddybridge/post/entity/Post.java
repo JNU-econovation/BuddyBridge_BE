6932172176dc4b2eb8cf49d5813437cc73974022
Profile-exe <kang67572346@gmail.com>
2025-01-23 17:47:17 +0900

    feat(Post): 댓글 검증에 모집완료된 게시글 검증 추가

--- src/main/java/econo/buddybridge/post/entity/Post.java
+++ src/main/java/econo/buddybridge/post/entity/Post.java
@@ -9,10 +9,12 @@ import econo.buddybridge.comment.exception.CommentSameGenderOnlyException;
 import econo.buddybridge.comment.exception.CommentSelfNotAllowedException;
 import econo.buddybridge.common.persistence.SoftDeletableEntity;
 import econo.buddybridge.matching.entity.Matching;
+import econo.buddybridge.matching.entity.MatchingStatus;
 import econo.buddybridge.member.entity.DisabilityType;
 import econo.buddybridge.member.entity.Gender;
 import econo.buddybridge.member.entity.Member;
 import econo.buddybridge.post.dto.PostUpdateReqDto;
+import econo.buddybridge.post.exception.CommentNotAllowedFinishedPostException;
 import econo.buddybridge.post.exception.PostDeleteNotAllowedException;
 import econo.buddybridge.post.exception.PostUnauthorizedAccessException;
 import econo.buddybridge.post.exception.PostUpdateNotAllowedException;
@@ -156,6 +158,12 @@ public class Post extends SoftDeletableEntity {
 
         if (this.gender != author.getGender()) {
             throw CommentSameGenderOnlyException.EXCEPTION;
+        }
+
+        // 상태 확인해 모집완료된 게시글에는 댓글 작성 불가
+        boolean isRecruiting = this.matchings.stream().allMatch(matching -> matching.getMatchingStatus() == MatchingStatus.PENDING);
+        if (!isRecruiting) {
+            throw CommentNotAllowedFinishedPostException.EXCEPTION;
         }
     }
 
